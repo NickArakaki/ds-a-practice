c6cec044cbe7f033781aa9541fcdb3f5075ee196
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-02 11:36:01 -0500

    add examples for isUnique

--- Arrays_and_Strings/IsUnique.js
+++ Arrays_and_Strings/IsUnique.js
@@ -1,1 +1,10 @@
 // Implement an algo to determine if a string has all unique characters. What if you cannot use additional data structures?
+function isUnique(string) {
+    return
+}
+
+console.log("1. true", isUnique('cat'))
+console.log("2. true", isUnique('water'))
+console.log("3. true", isUnique('dungeon'))
+console.log("4. false", isUnique('waterfall'))
+console.log("5. false", isUnique('llama'))
