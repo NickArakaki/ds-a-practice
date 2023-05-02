7af9103ab014b9917cb23b900a5c9116c44f2e8b
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-02 11:33:31 -0500

    add examples for permutations

--- Arrays_and_Strings/CheckPermutations.js
+++ Arrays_and_Strings/CheckPermutations.js
@@ -1,1 +1,9 @@
 // Given two strings, write a method to decide if one is a permutation of the other
+function isPermutation(s1, s2) {
+    return
+}
+
+console.log("1. true", isPermutation("cat", "tac"))
+console.log("2. true", isPermutation("dog", "god"))
+console.log("3. false", isPermutation("water", "fires"))
+console.log("4. false", isPermutation("", "hello"))
