a55345cbd939a2e3fe8e7beaacffd6172f373129
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-05 15:52:06 -0500

    add leetcode problems

---
+++ Arrays_and_Strings/longestPalindromeSubstring.js
@@ -0,0 +1,15 @@
+/*
+Given a string s, return the longest
+palindromic substring in s.
+
+Constraints
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.
+*/
+
+function longestPalindromeSubstring(string) {
+    //
+}
+
+console.log(longestPalindromeSubstring("babad")) // "bab"
+console.log(longestPalindromeSubstring("cbbd")) // "bb"
