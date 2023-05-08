01f747cd2f4881a25b3951042b26ec06e1d0e422
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-08 13:32:59 -0500

    refactored solution

--- LeetCode/219.ContainsDuplicateII.py
+++ LeetCode/219.ContainsDuplicateII.py
@@ -9,23 +9,44 @@ Constraints:
     0 <= k <= 105
 """
 
+# def containsNearbyDuplicate(nums, k):
+#     # initialize a set
+#     neighbors = set()
+#     # initialize a right and left pointer to 0
+#     left, right = 0, 0
+#     # iterate while right pointer not out of bounds
+#     while (right < len(nums)):
+#         # check the window to make sure it is less than or equal to k
+#         if right - left > k:
+#             neighbors.remove(nums[left])
+#             left += 1
+#         else:
+#             if nums[right] in neighbors:
+#                 return True
+#             else:
+#                 neighbors.add(nums[right])
+#                 right += 1
+#     # return False
+#     return False
+
+# refactored solution
 def containsNearbyDuplicate(nums, k):
     # initialize a set
-    neighbors = set()
+    window = set()
+
+    left = 0
     # initialize a right and left pointer to 0
-    left, right = 0, 0
     # iterate while right pointer not out of bounds
-    while (right < len(nums)):
-        # check the window to make sure it is less than or equal to k
+    for right in range(len(nums)):
+        # first check the length of the window
         if right - left > k:
-            neighbors.remove(nums[left])
+            window.remove(nums[left])
             left += 1
-        else:
-            if nums[right] in neighbors:
-                return True
-            else:
-                neighbors.add(nums[right])
-                right += 1
+
+        if nums[right] in window:
+            return True
+
+        window.add(nums[right])
     # return False
     return False
 
