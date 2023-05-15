9e814e008c53230cba0e9256dce18908631af584
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-15 18:32:39 -0500

    add topkfrequentelements

---
+++ LeetCode/347.TopKFrequentElements.py
@@ -0,0 +1,20 @@
+"""
+Given an integer array nums and an integer k,
+return the k most frequent elements.
+You may return the answer in any order.
+
+Constraints:
+    1 <= nums.length <= 105
+
+    -104 <= nums[i] <= 104
+
+    k is in the range [1, the number of unique elements in the array].
+
+    It is guaranteed that the answer is unique.
+"""
+
+def topKFrequent(nums, k):
+    pass
+
+print(topKFrequent([1,1,1,2,2,3], 2)) # [1, 2]
+print(topKFrequent([1], 1)) # [1]
