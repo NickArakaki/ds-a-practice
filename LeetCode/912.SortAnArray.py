4a53129416dd850c0b1c01e007fe0455305343eb
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-17 15:56:29 -0500

    add sort an array problem

---
+++ LeetCode/912.SortAnArray.py
@@ -0,0 +1,18 @@
+"""
+Given an array of integers nums, sort the array in
+ascending order and return it.
+
+You must solve the problem without using any built-in
+functions in O(nlog(n)) time complexity and with the
+smallest space complexity possible.
+
+Constraints:
+    1 <= nums.length <= 5 * 104
+    -5 * 104 <= nums[i] <= 5 * 104
+"""
+
+def sortArray(nums):
+    pass
+
+print(sortArray([5,2,3,1])) # [1,2,3,4,5]
+print(sortArray([5,1,1,2,0,0])) # [0,0,1,1,2,5]
