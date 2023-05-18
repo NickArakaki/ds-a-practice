e5cd87daf64075868b48a35e1ae7e06fe7c11f48
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-18 12:29:18 -0500

    add my n log n time complexity solution for sort chars by frequency

--- LeetCode/451.SortCharactersByFrequency.py
+++ LeetCode/451.SortCharactersByFrequency.py
@@ -12,7 +12,20 @@ Constraints:
 """
 
 def frequencySort(s):
-    pass
+    frequency_map = {}
+
+    for char in s:
+        frequency_map[char] = frequency_map.get(char, 0) + 1
+
+    # sorted_chars = freq_map.items().sort(key= lambda tup: tup[1], reverse=True)
+    freq_list = list(frequency_map.items())
+    freq_list.sort(key=lambda tup: tup[1], reverse=True)
+
+    res = []
+    for char, freq in freq_list:
+        res.append(char * freq)
+
+    return "".join(res)
 
 print(frequencySort("tree")) # "eert"
 print(frequencySort("cccaaa")) # "aaaccc"
