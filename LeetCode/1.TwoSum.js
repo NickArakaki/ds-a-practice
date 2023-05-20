290ccadb03c296655ac0604609b2cdfefae1cae5
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-19 18:00:57 -0700

    messing with prettier

--- LeetCode/1.TwoSum.js
+++ LeetCode/1.TwoSum.js
@@ -1,15 +1,15 @@
 function twoSum(nums, target) {
-    const targetNums = {}
+  const targetNums = {};
 
-    for (let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
 
-        if (diff in targetNums) return [targetNums[diff], i]
-        
-        targetNums[nums[i]] = i
-    }
+    if (diff in targetNums) return [targetNums[diff], i];
+
+    targetNums[nums[i]] = i;
+  }
 }
 
-console.log(twoSum([2,7,11,15], 9))
-console.log(twoSum([3,2,4], 6))
-console.log(twoSum([3,3], 6))
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
