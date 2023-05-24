829c87236c35e5a55e3682694ceaf946cd9b39c4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-24 13:36:36 -0500

    add better iterative solution

--- LeetCode/206.ReverseLinkedList.py
+++ LeetCode/206.ReverseLinkedList.py
@@ -17,22 +17,13 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 
 # Iterative Solution
 def revserSelis(head):
-    if head is None: # edge case if empty list
-        return
+    prev = None
 
-    # build a stack of nodes
-    node_stack = []
     curr_node = head
+    while curr_node:
+        next_node = curr_node.next
+        curr_node.next = prev
+        prev = curr_node
+        curr_node = next_node
 
-    while (curr_node):
-        node_stack.append(curr_node)
-        curr_node = curr_node.next
-
-    new_head = node_stack.pop()
-    prev_node = new_head
-    while(len(node_stack)):
-        curr_node = node_stack.pop()
-        prev_node.next = curr_node
-        prev_node = curr_node
-
-    return new_head
+    return prev
