c0f78dbc0b8e80141ebc033475b9b632a024797e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-24 12:48:50 -0500

    add search in rotated array solution

--- LeetCode/33.SearchInRotatedSortedArray.py
+++ LeetCode/33.SearchInRotatedSortedArray.py
@@ -24,30 +24,51 @@ nums is an ascending array that is possibly rotated.
         # list is not rotated
         # max num times rotated? going to assume 1 for this problem
 
+# def search(nums: list[int], target: int) -> int:
+#     # everything in the left half of the array will be greater than the values in the right half
+#     l, r = 0, len(nums) - 1
+
+#     while l <= r:
+#         mid = l + (r - l) // 2
+#         # print(nums[mid])
+
+#         if nums[mid] == target:
+#             return mid
+
+#         if nums[l] <= nums[mid]: # if in left sorted
+#             if target > nums[mid] or target < nums[l]:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+#         else:  # if in right sorted
+#             if target < nums[mid] or target > nums[r]:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+
+#     return -1
+
+
 def search(nums: list[int], target: int) -> int:
-    # everything in the left half of the array will be greater than the values in the right half
     l, r = 0, len(nums) - 1
 
     while l <= r:
-        mid = l + (r - l) // 2
-        # print(nums[mid])
+        mid = (r + l) // 2
 
-        if nums[mid] == target:
-            return mid
+        if nums[mid] == target: return mid
 
-        if nums[l] <= nums[mid]: # if in left sorted
-            if target > nums[mid] or target < nums[l]:
+        if nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]: # discard left
                 l = mid + 1
-            else:
+            else: # discard right
                 r = mid - 1
-        else:  # if in right sorted
-            if target < nums[mid] or target > nums[r]:
+        else:
+            if target > nums[mid] or target > nums[r]: # discard right
                 r = mid - 1
             else:
                 l = mid + 1
 
     return -1
-
 
 print(search([4,5,6,7,0,1,2], 0)) # 4
 print(search([4,5,6,7,0,1,2], 3)) # -1
