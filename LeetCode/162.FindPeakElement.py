7ad85d150401b9395a936af81e4d29ba8f694b21
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-25 15:10:15 -0500

    add recursive solution for find peak element

--- LeetCode/162.FindPeakElement.py
+++ LeetCode/162.FindPeakElement.py
@@ -18,17 +18,31 @@ Constraints
 nums[i] != nums[i + 1] for all valid i.
 """
 
-def findPeakElement(nums:list[int]) -> int:
-    l, r = 0, len(nums) - 1
+# def findPeakElement(nums:list[int]) -> int:
+#     l, r = 0, len(nums) - 1
 
-    while l <= r:
-        mid = l + (r - l) // 2
-        if mid > 0 and nums[mid] < nums[mid - 1]: # search left
-            r = mid - 1
-        elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]: # search right
-            l = mid + 1
-        else:
-            return mid
+#     while l <= r:
+#         mid = l + (r - l) // 2
+#         if mid > 0 and nums[mid] < nums[mid - 1]: # search left
+#             r = mid - 1
+#         elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]: # search right
+#             l = mid + 1
+#         else:
+#             return mid
+
+# recursive solution
+def findPeakElement(nums: list[int]) -> int:
+    if len(nums) == 1: return 0
+    if len(nums) == 2: return 0 if nums[0] > nums[1] else 1
+
+    mid = len(nums) // 2
+
+    if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]: return mid
+    if nums[mid + 1] > nums[mid]:
+        return mid + findPeakElement(nums[mid:])
+    else:
+        return findPeakElement(nums[:mid])
+
 
 print(findPeakElement([1,2,3,1])) # 2
 print(findPeakElement([1,2,1,3,5,6,4])) # 5
