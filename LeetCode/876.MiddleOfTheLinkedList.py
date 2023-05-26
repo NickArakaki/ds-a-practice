cb6555306ba9e5aeb4d3c6fb705664f97de4015c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-05-26 13:45:05 -0500

    add find middle node O(n) solution

--- LeetCode/876.MiddleOfTheLinkedList.py
+++ LeetCode/876.MiddleOfTheLinkedList.py
@@ -12,4 +12,11 @@ Constraints:
 """
 
 def middleNode(head):
-    pass
+    # fast and slow pointers
+    fast, slow = head, head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
