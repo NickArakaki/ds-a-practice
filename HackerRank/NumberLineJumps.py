1e62cf37e8e3a9de34bbe344493d69fcaf487802
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-02 13:19:36 -0500

    add recursive solution

--- HackerRank/NumberLineJumps.py
+++ HackerRank/NumberLineJumps.py
@@ -1,12 +1,20 @@
+# def kangaroo(x1, v1, x2, v2):
+#     # Write your code here
+#     while True:
+#         if x1 == x2:
+#             return "YES"
+#         elif (x1 > x2 and v1 >= v2) or (x2 > x1 and v2 >= v1):
+#             return "NO"
+
+#         x1 += v1
+#         x2 += v2
+
 def kangaroo(x1, v1, x2, v2):
     # Write your code here
-    while True:
-        if x1 == x2:
-            return "YES"
-        elif (x1 > x2 and v1 >= v2) or (x2 > x1 and v2 >= v1):
-            return "NO"
+    if x1 == x2: return "YES"
+    if (x1 > x2 and v1 >= v2) or (x2 > x1 and v2 >= v1): return "NO"
+    x1 += v1
+    x2 += v2
+    return kangaroo(x1, v1, x2, v2)
 
-        x1 += v1
-        x2 += v2
-
-print(kangaroo(1,1,2,2)) # YES
+print(kangaroo(2,1,1,2)) # YES
