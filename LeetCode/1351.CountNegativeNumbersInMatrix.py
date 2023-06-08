0b29f6dddee1d16d7ed7630895d1d7f967281c31
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-08 14:14:07 -0500

    add O(m log m) solution

--- LeetCode/1351.CountNegativeNumbersInMatrix.py
+++ LeetCode/1351.CountNegativeNumbersInMatrix.py
@@ -4,13 +4,33 @@ order both row-wise and column-wise, return the number of negative
 numbers in the grid
 """
 
+# def countNegatives(grid: list[list[int]]) -> int:
+#     count = 0
+
+#     for r_idx in range(len(grid)):
+#         for c_idx in range(len(grid[0])):
+#             curr_value = grid[r_idx][c_idx]
+#             if curr_value < 0: count += 1
+
+#     return count
+
 def countNegatives(grid: list[list[int]]) -> int:
     count = 0
 
-    for r_idx in range(len(grid)):
-        for c_idx in range(len(grid[0])):
-            curr_value = grid[r_idx][c_idx]
-            if curr_value < 0: count += 1
+    for row in grid:
+        # BS for each row to find first instance of value < 0
+        l, r = 0, len(row) - 1
+        first_neg = None
+        while l <= r:
+            mid = (r + l)  // 2
+            if row[mid] < 0: # need to check vals to the left
+                first_neg = mid
+                r = mid - 1
+            else: # need to check vals to right
+                l = mid + 1
+
+        if first_neg is not None:
+            count += len(row) - first_neg
 
     return count
 
