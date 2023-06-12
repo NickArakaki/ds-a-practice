6a40f5317ec3bc8fe2b2e852bcf2d69bdf4cb9b0
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-12 17:14:48 -0500

    working on iterative solution

---
+++ LeetCode/94.BinaryTreeInorderTraversal.py
@@ -0,0 +1,22 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+"""
+
+def inorderTraversal(root):
+    # dfs
+    res = []
+    stack = [root]
+    curr_node = root
+
+    while len(stack):
+        if curr_node.left:
+            stack.append(curr_node.left)
+            curr_node = curr_node.left
+        else:
+            curr_node = stack.pop()
+            res.append(curr_node.val)
+            if curr_node.right:
+                stack.append(curr_node.right)
+                curr_node = stack.right
+
+    return res
