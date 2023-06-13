396b5d7ef3845440cd879dfb1624da61dcd58527
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-13 14:02:51 -0500

    add euql pairs problem

---
+++ LeetCode/2351.EqualRowAndColumnPairs.py
@@ -0,0 +1,11 @@
+"""
+Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+"""
+
+def equalPairs(grid: list[list[int]]) -> int:
+    pass
+
+print(equalPairs([[3,2,1],[1,7,6],[2,7,7]])) # 1
+print(equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]])) # 3
