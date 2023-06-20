8e5f1c441abe8d3e6f23e63906412b3a7e59e5f0
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-20 17:23:46 -0500

    add O(n) solution

--- LeetCode/2090.KRadiusSubarrayAverages.py
+++ LeetCode/2090.KRadiusSubarrayAverages.py
@@ -15,8 +15,34 @@ For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4 
 """
 
 def getAverages(nums: list[int], k: int) -> list[int]:
-    pass
+    # determine if there are at least k elements on either side of mid
+    if k == 0: return nums
+
+    left_sum, right_sum = 0, sum(nums[:k + 1])
+    l, r = 0, k + 1
+    res = []
+
+    for i, num in enumerate(nums):
+        num_el_left = i - 0
+        num_el_right = len(nums) - i - 1
+        if num_el_left < k or num_el_right < k:
+            res.append(-1)
+        else:
+            avg = (left_sum + right_sum) // (k * 2 + 1)
+            res.append(avg)
+            left_sum -= nums[l]
+            l += 1
+
+        if r < len(nums):
+            right_sum += nums[r]
+            r += 1
+
+        left_sum += num
+        right_sum -= num
+    return res
 
 print(getAverages([7,4,3,9,1,8,5,2,6], 3)) # [-1,-1,-1,5,4,4,-1,-1,-1]
-print(getAverages([100000]), 0) # [100000]
-print(getAverages([8]), 100000) # [-1]
+print(getAverages([100000], 0)) # [100000]
+print(getAverages([8], 100000)) # [-1]
+print(getAverages([8, 1, 3], 1)) # [-1, 4, -1]
+print(getAverages([8, 2], 100000)) # [-1, -1]
