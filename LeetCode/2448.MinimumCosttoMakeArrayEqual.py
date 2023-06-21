c722af78a98eb2b14fedc369993bec4481808704
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-21 12:42:33 -0500

    add min cost to make array equal problem

---
+++ LeetCode/2448.MinimumCosttoMakeArrayEqual.py
@@ -0,0 +1,16 @@
+"""
+You are given two 0-indexed arrays nums and cost consisting each of n positive integers.
+
+You can do the following operation any number of times:
+
+Increase or decrease any element of the array nums by 1.
+The cost of doing one operation on the ith element is cost[i].
+
+Return the minimum total cost such that all the elements of the array nums become equal.
+"""
+
+def minCost(nums: list[int], cost: list[int]) -> int:
+    pass
+
+print(minCost([1,3,5,2], [2,3,1,14])) # 8
+print(minCost([2,2,2,2,2], [4,2,8,1,3])) # 0
