bdd93d285e17b7caa77b0e307cb74a2bf88e6895
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-22 11:24:01 -0500

    add solution

--- HackerRank/RecursiveDigitSum.py
+++ HackerRank/RecursiveDigitSum.py
@@ -1,6 +1,10 @@
 def superDigit(n,k):
-    pass
+    super_dig = sum([int(s) for s in n]) * k
+    if super_dig < 10:
+        return super_dig
+    else:
+        return superDigit(str(super_dig), 1)
 
-print(superDigit(9875, 4)) # 8
-print(superDigit(148, 3)) # 3
-print(superDigit(123, 3)) # 9
+print(superDigit('9875', 4)) # 8
+print(superDigit('148', 3)) # 3
+print(superDigit('123', 3)) # 9
