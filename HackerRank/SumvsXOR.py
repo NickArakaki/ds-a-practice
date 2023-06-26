628c7db55b18a6d08f6abbff8b287770741cc52c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-26 10:25:19 -0500

    add logn solution

--- HackerRank/SumvsXOR.py
+++ HackerRank/SumvsXOR.py
@@ -1,11 +1,23 @@
+# def sumXor(n):
+#     num_equal = 0
+
+#     for i in range(n + 1):
+#         num_sum = n + i
+#         num_xor = n ^ i
+#         if num_sum == num_xor:
+#             num_equal += 1
+
+#     return num_equal
+
 def sumXor(n):
     num_equal = 0
 
-    for i in range(n + 1):
-        num_sum = n + i
-        num_xor = n ^ i
-        if num_sum == num_xor:
+    while n > 0:
+        if n % 2 == 0:
             num_equal += 1
+        n //= 2
+
+    num_equal = 2 ** num_equal
 
     return num_equal
 
