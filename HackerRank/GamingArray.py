4302596db2e1079ea5c61d000d0bff5d4af17866
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-26 10:43:36 -0500

    add solution for gaming array

--- HackerRank/GamingArray.py
+++ HackerRank/GamingArray.py
@@ -1,5 +1,27 @@
 def gamingArray(arr):
-    pass
+
+    def _map_array(array):
+        array_map = {}
+        curr_max = float('-inf')
+        curr_max_index = None
+
+        for idx, el in enumerate(array):
+            if el > curr_max:
+                curr_max = el
+                curr_max_index = idx
+            array_map[idx] = curr_max_index
+
+        return array_map
+
+    arr_map = _map_array(arr)
+    turn = 0
+
+    while arr:
+        max_idx = arr_map[len(arr) - 1]
+        arr = arr[:max_idx]
+        turn += 1
+
+    return "BOB" if turn % 2 == 1 else "ANDY"
 
 
 print(gamingArray([2,3,5,4,1])) # BOB
