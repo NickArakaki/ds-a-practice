9b2ffeb22a9b51d90bd2d76f987f4500df298467
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-28 11:03:37 -0500

    add generate parenthesis

---
+++ LeetCode/22.GenerateParantheses.py
@@ -0,0 +1,9 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+"""
+
+def generate_parenthesis(n: int) -> list[str]:
+    pass
+
+print(generate_parenthesis(3)) # ["((()))","(()())","(())()","()(())","()()()"]
+print(generate_parenthesis(1)) # ["()"]
