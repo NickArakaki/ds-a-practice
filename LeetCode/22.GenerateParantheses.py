a17267189085acad458aae4a57659eaf446a8507
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-28 11:30:04 -0500

    add generate paranthesis

--- LeetCode/22.GenerateParantheses.py
+++ LeetCode/22.GenerateParantheses.py
@@ -3,7 +3,30 @@ Given n pairs of parentheses, write a function to generate all combinations of w
 """
 
 def generate_parenthesis(n: int) -> list[str]:
-    pass
+    ans = []
+
+    def _backtrack(num_open, num_closed, combo):
+        if len(combo) == 2 * n:
+            ans.append("".join(combo))
+            return
+
+        # decision to add open
+            # if num open < n
+        if num_open < n and len(combo) < 2*n - 1:
+            combo.append("(")
+            _backtrack(num_open + 1, num_closed, combo)
+            combo.pop()
+
+        # decision to add closed
+            # only if num open > num closed and num closed < n
+        if num_open > num_closed and num_closed < n:
+            combo.append(")")
+            _backtrack(num_open, num_closed + 1, combo)
+            combo.pop()
+
+    _backtrack(1, 0, ["("])
+
+    return ans
 
 print(generate_parenthesis(3)) # ["((()))","(()())","(())()","()(())","()()()"]
 print(generate_parenthesis(1)) # ["()"]
