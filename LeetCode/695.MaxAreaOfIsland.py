9f555e6c7c83fe52c2787366176b592264b547b1
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-29 13:16:12 -0500

    addd max area of island

---
+++ LeetCode/695.MaxAreaOfIsland.py
@@ -0,0 +1,19 @@
+"""
+You are given an m x n binary matrix grid. An island is a
+group of 1's (representing land) connected 4-directionally
+(horizontal or vertical.) You may assume all four edges of the
+grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+"""
+
+def max_area_of_island(grid: list[list[int]]) -> int:
+    pass
+
+
+print(max_area_of_island([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]])) # 6
+print(max_area_of_island([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]) == 6) # 6
+print(max_area_of_island([[0,0,0,0,0,0,0,0]])) # 0
+print(max_area_of_island([[0,0,0,0,0,0,0,0]]) == 0) # 0
