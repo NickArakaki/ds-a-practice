53a52659a6fd74ac5eb98b83ec6934e6ec915999
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-06-29 13:41:54 -0500

    working on max area of island

--- LeetCode/695.MaxAreaOfIsland.py
+++ LeetCode/695.MaxAreaOfIsland.py
@@ -10,7 +10,41 @@ Return the maximum area of an island in grid. If there is no island, return 0.
 """
 
 def max_area_of_island(grid: list[list[int]]) -> int:
-    pass
+
+    def _dfs(row, col, curr_island):
+        if not _in_bounds(row, col):
+            return
+
+        pos = (row, col)
+        if pos in visited:
+            return
+
+        if grid[row][col] == 0:
+            return
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for direction in directions:
+            new_row = row + direction[0]
+            new_col = col + direction[1]
+            _dfs(new_row, new_col, curr_island)
+
+        return
+
+
+    def _in_bounds(row, col):
+        row_inbound = row >= 0 and row < len(grid)
+        col_inbound = col >= 0 and col < len(grid[0])
+        return row_inbound and col_inbound
+
+
+    max_island = 0
+    visited = set()
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            curr_area = _dfs(row, col, 0)
+
+    return max_island
 
 
 print(max_area_of_island([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]])) # 6
