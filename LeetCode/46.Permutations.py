a95c38997015517c4df73dc150c59996d1607937
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-04 13:07:38 -0500

    add backtrack solution

--- LeetCode/46.Permutations.py
+++ LeetCode/46.Permutations.py
@@ -4,8 +4,21 @@ You can return the answer in any order.
 """
 
 def permute(nums: list[int]) -> list[list[int]]:
-    pass
+    res = []
+    if len(nums) == 1:
+        return [nums.copy()]
 
+    for _ in range(len(nums)):
+        num = nums.pop(0)
+        permutations = permute(nums)
+
+        for permutation in permutations:
+            permutation.append(num)
+
+        nums.append(num)
+        res.extend(permutations)
+
+    return res
 
 print(permute([1,2,3]) == [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
 print(permute([0,1]) == [[0,1],[1,0]])
