aa191c805d12ccfa211989161125f8e5bb496f3d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-04 12:49:15 -0500

    add permutations

---
+++ LeetCode/46.Permutations.py
@@ -0,0 +1,12 @@
+"""
+Given an array nums of distinct integers, return all the possible permutations.
+You can return the answer in any order.
+"""
+
+def permute(nums: list[int]) -> list[list[int]]:
+    pass
+
+
+print(permute([1,2,3]) == [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
+print(permute([0,1]) == [[0,1],[1,0]])
+print(permute([1]) == [[1]])
