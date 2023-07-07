330eed22f0b7331e0732174360c51f2c38b9c0e1
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-07 11:14:41 -0500

    add partial solution

--- HackerRank/SherlockValidString.py
+++ HackerRank/SherlockValidString.py
@@ -5,7 +5,30 @@ the same number of times. Given a string , determine if it is valid. If so, retu
 """
 
 def is_valid(s: str) -> str:
-    pass
+    # get character count in s
+    char_count = {}
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+
+    # create a flag to track if removed
+    has_removed = False
+
+    # iterate through char count values sorted
+    sorted_count = sorted(char_count.values())
+    for r in range(1, len(sorted_count)):
+        prev_count = sorted_count[r - 1]
+        curr_count = sorted_count[r]
+
+        if curr_count != prev_count and not has_removed:
+            sorted_count[r] -= 1
+            curr_count -= 1
+            has_removed = True
+
+        if curr_count != prev_count and has_removed:
+            return "NO"
+
+    return "YES"
+
 
 print(is_valid("abcdefghhgfedecba") == "YES")
 print(is_valid("aabbcd") == "NO")
