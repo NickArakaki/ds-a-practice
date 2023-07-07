e369b78a59fd36f2c04c4d1286cad78183e7f1e2
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-07 13:21:49 -0500

    wokring on linked list class practice

--- LeetCode/206.ReverseLinkedList.py
+++ LeetCode/206.ReverseLinkedList.py
@@ -16,14 +16,59 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 #     return _helper(prev, head)
 
 # Iterative Solution
-def revserSelis(head):
-    prev = None
+# def revserSelis(head):
+#     prev = None
 
-    curr_node = head
-    while curr_node:
-        next_node = curr_node.next
-        curr_node.next = prev
-        prev = curr_node
-        curr_node = next_node
+#     curr_node = head
+#     while curr_node:
+#         next_node = curr_node.next
+#         curr_node.next = prev
+#         prev = curr_node
+#         curr_node = next_node
 
-    return prev
+#     return prev
+
+
+class Node:
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
+
+
+    def __repr__ (self):
+        return f'value = {self.val}'
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+        self.tail = Node()
+
+        self.head.next = self.tail
+
+        self.length = 0
+
+
+    def append(self, node):
+        curr_node = self.head
+        while curr_node.next:
+            curr_node = curr_node.next
+
+        curr_node.next = node
+        self.length += 1
+
+
+    def reverse(self):
+        pass
+
+
+    def print_list(self):
+        curr_node = self.head
+        while curr_node.next:
+            curr_node = curr_node.next
+            print(curr_node)
+
+l_list = LinkedList()
+l_list.append(Node(1))
+l_list.append(Node(3))
+l_list.print_list()
