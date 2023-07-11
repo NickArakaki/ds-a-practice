25519181799775c62208f5cea4f73de6c991ad39
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-11 16:30:28 -0500

    add list node class to swap in pairs problem

--- LeetCode/24.SwapNodesInPairs.py
+++ LeetCode/24.SwapNodesInPairs.py
@@ -4,6 +4,15 @@ and return its head. You must solve the problem
 without modifying the values in the list's nodes
 (i.e., only nodes themselves may be changed.)
 """
+class ListNode():
+    def __init__(self, val=0, next=None):
+        self.val = 0
+        self.next = next
+
+
+    def __repr__(self) -> str:
+        return f'value = {self.val}'
+
 
 def swap_pairs(head):
     dummy_node = ListNode(None)
