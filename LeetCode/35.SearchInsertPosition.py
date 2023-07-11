dd7ec82382c35367593f96bf89379155c5559df7
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-11 17:01:01 -0500

    add binary search solution

--- LeetCode/35.SearchInsertPosition.py
+++ LeetCode/35.SearchInsertPosition.py
@@ -8,18 +8,17 @@ You must write an algorithm with O(log n) runtime complexity.
 
 def search_insert(nums: list[int], target: int) -> int:
     l, r = 0, len(nums) - 1
-    mid = 0
 
     while l <= r:
         mid = (l + r) // 2
         if nums[mid] == target:
             return mid
         elif nums[mid] > target:
-            r = mid
+            r = mid - 1
         else:
             l = mid + 1
 
-    return mid
+    return l
 
 print(search_insert([1,3,5,6], 5) == 2)
 print(search_insert([1,3,5,6], 2) == 1)
