99fd7dce6af40588807f68e70122f66ef669f83d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-18 12:38:12 -0500

    outline general strategy

--- LeetCode/19.RemoveNthNodeFromEndOfList.py
+++ LeetCode/19.RemoveNthNodeFromEndOfList.py
@@ -4,4 +4,10 @@ Given the head of a linked list, remove the nth node from the end of the list an
 from Helpers import LinkedList
 
 def removeNthFromEnd(head, n):
+    #
+    # iterate through list to get length of list
+    # subtract n from length to get node to remove
+    # iterate through list up to node to be removed
+    # remove node
+    # return head
     pass
