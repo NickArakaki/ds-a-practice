f3a740c478ed1abe253653ada2fe05ef905826b6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-07-18 12:55:21 -0500

    add two pointer solution plan

--- LeetCode/19.RemoveNthNodeFromEndOfList.py
+++ LeetCode/19.RemoveNthNodeFromEndOfList.py
@@ -4,11 +4,9 @@ Given the head of a linked list, remove the nth node from the end of the list an
 from Helpers import LinkedList
 
 def removeNthFromEnd(head, n):
-    # initialize counter to get length of list
-    # initialize dummy node to point to head of linked list
-    # iterate through list to get length of list
-    # subtract n from length to get node to remove
-    # iterate through list up to node to be removed
-    # remove node
-    # return next node from dummy
+    # initialzie a dummy node with next value as head of llist
+    # ititialize a pointer to be n nodes away from head
+    # iterate while r pointer has a valid node
+    # once at the end remove the l pointer (head-pointer)
+    # return next node from dummy node
     pass
