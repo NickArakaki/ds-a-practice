55431e2de0c68dfe7d5685bd00ad3882f0dedcd6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-07 11:25:27 -0500

    use original solution

--- LeetCode/345.ReverseVowelsOfAString.py
+++ LeetCode/345.ReverseVowelsOfAString.py
@@ -10,24 +10,24 @@ def reverse_vowels(s: str) -> str:
     l, r = 0, len(chars) - 1
 
     while l < r:
-        while l < len(chars) and chars[l] not in vowels:
-            l += 1
-        while r >= 0 and chars[r] not in vowels:
-            r -=1
-
-        if l < r:
-            chars[l], chars[r] = chars[r], chars[l]
-            l += 1
-            r -=1
-
-        # if chars[l] not in vowels:
+        # while l < len(chars) and chars[l] not in vowels:
         #     l += 1
-        # if chars[r] not in vowels:
-        #     r -= 1
-        # if chars[l] in vowels and chars[r] in vowels:
+        # while r >= 0 and chars[r] not in vowels:
+        #     r -=1
+
+        # if l < r:
         #     chars[l], chars[r] = chars[r], chars[l]
         #     l += 1
-        #     r -= 1
+        #     r -=1
+
+        if chars[l] not in vowels:
+            l += 1
+        if chars[r] not in vowels:
+            r -= 1
+        if chars[l] in vowels and chars[r] in vowels:
+            chars[l], chars[r] = chars[r], chars[l]
+            l += 1
+            r -= 1
 
     return "".join(chars)
 
