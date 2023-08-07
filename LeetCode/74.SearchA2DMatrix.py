48793662b663e30113c597c0c466eb6e8ab44392
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-07 10:31:59 -0500

    cleaned up code

--- LeetCode/74.SearchA2DMatrix.py
+++ LeetCode/74.SearchA2DMatrix.py
@@ -9,28 +9,29 @@ You must write a solution in O(log(m * n)) time complexity.
 """
 
 def search_matrix(matrix: list[list[int]], target: int) -> bool:
-    # bin search through first level of lists
-    # bin search within the list that could contain target
-        # return true if found
-    # return false if not found after bin searches
     l1, r1 = 0, len(matrix) - 1
-    while l1 < r1: # outer bin search
+
+    while l1 < r1:
         mid1 = (l1 + r1) // 2
         curr_list = matrix[mid1]
+
         if curr_list[0] <= target and curr_list[-1] >= target:
-            # do inner bin search
+
             l2, r2 = 0, len(curr_list) - 1
+
             while l2 < r2:
                 mid2 = (l2 + r2) // 2
                 if curr_list[mid2] == target:
                     return True
-                elif target < curr_list[mid2]: # search left
+                elif target < curr_list[mid2]:
                     r2 = mid2
-                else:
+                elif target > curr_list[mid2]:
                     l2 = mid2 + 1
-        elif target < curr_list[-1]: # search left
+            return False
+
+        elif target < curr_list[-1]:
             r1 = mid1
-        else:
+        elif target > curr_list[-1]:
             l1 = mid1 + 1
     return False
 
