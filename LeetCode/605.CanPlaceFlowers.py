094fe12a05d8b2a6f2ca669eb48717f9f21c3ea6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-08 13:37:00 -0500

    initial soltution

--- LeetCode/605.CanPlaceFlowers.py
+++ LeetCode/605.CanPlaceFlowers.py
@@ -8,9 +8,21 @@ the no-adjacent-flowers rule and false otherwise.
 """
 
 def can_place_flowers(flowerbed: list[int], n: int) -> bool:
-    pass
+    # iterate over flowerbed
+    i = 0
+    while n > 0 and i < len(flowerbed):
+        if flowerbed[i] == 0:
+            if len(flowerbed) == 1 or (i == 0 and flowerbed[i + 1] == 0) or (i == len(flowerbed) - 1 and flowerbed[i - 1] == 0) or (flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+        i += 1
+
+    return True if n <= 0 else False
 
 
 print(can_place_flowers([1,0,0,0,1], 1) == True)
 print(can_place_flowers([1,0,0,0,1], 2) == False)
 print(can_place_flowers([0,0,1,0,1], 1) == True)
+print(can_place_flowers([1,0,1,0,0], 1) == True)
+print(can_place_flowers([0], 1) == True)
+print(can_place_flowers([0], 2) == False)
