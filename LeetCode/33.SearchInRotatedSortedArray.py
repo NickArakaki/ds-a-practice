f3f9352cb2297cb7c5b32fb193668bfc592a3766
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-08 10:59:28 -0500

    add initial solution

--- LeetCode/33.SearchInRotatedSortedArray.py
+++ LeetCode/33.SearchInRotatedSortedArray.py
@@ -9,10 +9,25 @@ You must write an algorithm with O(log n) runtime complexity.
 """
 
 def search(nums: list[int], target: int) -> int:
-    pass
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
+        num = nums[mid]
+
+        if target == num:
+            return mid
+        elif target < num and target > nums[r]: # search left
+            r = mid - 1
+        elif target < num and target < nums[r]: # search right
+            l = mid + 1
+
+
+    return -1
 
 
 print(search([4,5,6,7,0,1,2], 0) == 4)
 print(search([4,5,6,7,0,1,2], 3) == -1)
+print(search([1,2,3,4,5,6,7], 1) == 0)
 print(search([1], 0) == -1)
-print(search([], 0) == -1)
+print(search([0], 0) == 0)
