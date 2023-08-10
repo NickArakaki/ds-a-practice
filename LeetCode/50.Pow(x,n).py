90ad0451396b28de3cbfead564f986f3c02db7c9
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-10 12:45:07 -0500

    add pow)

---
+++ LeetCode/50.Pow(x,n).py
@@ -0,0 +1,11 @@
+"""
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+"""
+
+def pow(x: float, n: int) -> float:
+    pass
+
+
+print(pow(2.00000, 10) == 1024.00000)
+print(pow(2.10000, 3) == 9.26100)
+print(pow(2.00000, -2) == 0.25000)
