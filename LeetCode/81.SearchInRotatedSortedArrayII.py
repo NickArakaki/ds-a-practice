677a413fa721da33a7c18042219189d91f2bed66
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-10 11:20:56 -0500

    add modified solution

--- LeetCode/81.SearchInRotatedSortedArrayII.py
+++ LeetCode/81.SearchInRotatedSortedArrayII.py
@@ -12,8 +12,33 @@ You must decrease the overall operation steps as much as possible.
 """
 
 def search(nums: list[int], target: int) -> bool:
-    pass
+    if len(nums) == 1:
+        return nums[0] == target
 
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        while l < r and nums[l] == nums[l + 1]:
+            l += 1
+        while r > l and nums[r] == nums[r - 1]:
+            r -= 1
+
+        mid = l + (r - l) // 2
+
+        if target == nums[mid]:
+            return True
+        elif nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+    return False
 
 print(search([2,5,6,0,0,1,2], 0) == True)
 print(search([2,5,6,0,0,1,2], 3) == False)
