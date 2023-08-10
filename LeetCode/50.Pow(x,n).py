804b8cbc57e4cafee889b73d07bf64fba5549499
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-10 13:23:43 -0500

    add log n solution

--- LeetCode/50.Pow(x,n).py
+++ LeetCode/50.Pow(x,n).py
@@ -2,13 +2,13 @@
 Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 """
 
-def pow(x: float, n: int) -> float:
-    ans = 1
+# def pow(x: float, n: int) -> float:
+#     ans = 1
 
-    for _ in range(abs(n)):
-        ans *= x
+#     for _ in range(abs(n)):
+#         ans *= x
 
-    return ans if n > 0 else 1 / ans
+#     return ans if n > 0 else 1 / ans
 
 # def pow(x: float, n: int) -> float:
 #     ans = my_pow(x, abs(n))
@@ -24,6 +24,27 @@ def pow(x: float, n: int) -> float:
 #     temp *= temp
 
 #     return temp if n % 2 == 0 else temp * x
+
+
+def my_pow(x: float, n: int) -> float:
+    if n == 0:
+        return 1
+
+    ans = solve(x, abs(n))
+
+    return ans if n > 0 else 1 / ans
+
+
+def solve(x: float, n: int) -> float:
+    ans = 1
+
+    while n > 0:
+        if n & 1: # odd
+            ans *= x
+        x *= x
+        n //= 2
+    return ans
+
 
 print(pow(2.00000, 10)) # 1024.00000
 print(pow(2.10000, 3)) # 9.26100
