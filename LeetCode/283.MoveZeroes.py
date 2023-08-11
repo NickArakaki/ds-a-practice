23831a9935d5326f2467aded0df216114979d9a7
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-11 15:30:15 -0500

    working on move zeroes

--- LeetCode/283.MoveZeroes.py
+++ LeetCode/283.MoveZeroes.py
@@ -6,9 +6,18 @@ Note that you must do this in-place without making a copy of the array.
 """
 
 def move_zeroes(nums: list[int]) -> list[int]:
+    r = 0
+    for l in range(len(nums)):
+        while r < len(nums) and nums[r] == 0:
+            r += 1
 
-    pass
+        if nums[l] == 0 and l < r and r < len(nums):
+            nums[l], nums[r] = nums[r], nums[l]
+            r += 1
+    print(nums)
+    return nums
 
-print(move_zeroes([[0,1,0,3,12]]) == [1,3,12,0,0])
-print(move_zeroes([[0]]) == [0])
-print(move_zeroes([[1, 0]]) == [1, 0])
+print(move_zeroes([0,1,0,3,12]) == [1,3,12,0,0])
+print(move_zeroes([1,3,0,12,0]) == [1,3,12,0,0])
+print(move_zeroes([0]) == [0])
+print(move_zeroes([1, 0]) == [1, 0])
