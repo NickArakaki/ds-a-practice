b9194d4fd5d3060bce98f612ee42d811cecdbdc9
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-14 14:29:59 -0500

    add n log k solution

--- LeetCode/215.KthLargetstElementInAnArray.py
+++ LeetCode/215.KthLargetstElementInAnArray.py
@@ -6,10 +6,21 @@ Note that it is the kth largest element in the sorted order, not the kth distinc
 Can you solve it without sorting?
 """
 
+import heapq
+
 def find_kth_largest(nums: list[int], k: int) -> int:
-    pass
+    # heapify nums
+    heap = nums[:k]
+    heapq.heapify(heap)
+
+    for num in nums:
+        if num > heap[0]:
+            heapq.heappop(heap)
+            heapq.heappush(heap, num)
+
+    return heap[0]
 
 
-print(find_kth_largest())
-print(find_kth_largest())
-print(find_kth_largest())
+print(find_kth_largest([3,2,1,5,6,4], 2) == 5)
+print(find_kth_largest([3,2,3,1,2,4,5,5,6], 4) == 4)
+print(find_kth_largest([1], 1) == 1)
