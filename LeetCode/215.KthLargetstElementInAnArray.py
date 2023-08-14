8a93f221a50085d57e1a8772f8f5c9010596ca7a
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-14 12:40:45 -0500

    add kth largest ele

---
+++ LeetCode/215.KthLargetstElementInAnArray.py
@@ -0,0 +1,15 @@
+"""
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+"""
+
+def find_kth_largest(nums: list[int], k: int) -> int:
+    pass
+
+
+print(find_kth_largest())
+print(find_kth_largest())
+print(find_kth_largest())
