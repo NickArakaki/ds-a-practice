a7976779402df200aa69550eb74f9e036360863b
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-15 13:52:38 -0500

    add two list solution to partition list

--- LeetCode/86.PartitionList.py
+++ LeetCode/86.PartitionList.py
@@ -7,4 +7,20 @@ You should preserve the original relative order of the nodes in each of the two 
 from Helpers import LinkedList
 
 def partition(head, x: int):
-    pass
+    before_head, after_head = LinkedList.SinglyLinkedListNode(), LinkedList.SinglyLinkedListNode()
+    before_curr, after_curr = before_head, after_head
+
+    while head:
+        if head.val < x:
+            before_curr.next = head
+            before_curr = before_curr.next
+        else:
+            after_curr.next = head
+            after_curr = after_curr.next
+
+        head = head.next
+
+    after_curr.next = None
+    before_curr.next = after_head.next
+
+    return before_head.next
