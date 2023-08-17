aec1baa969f6d2df77bc2274a80d7b30fbfa51c4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-17 12:01:29 -0500

    add recursive solution for check for valid parantheses

--- LeetCode/2369.CheckIfThereIsAValidPartitionForTheArray.py
+++ LeetCode/2369.CheckIfThereIsAValidPartitionForTheArray.py
@@ -12,7 +12,27 @@ Return true if the array has at least one valid partition. Otherwise, return fal
 """
 
 def valid_partition(nums: list[int]) -> bool:
-    pass
+    cache = {}
+
+    def dfs(i):
+        if i == len(nums):
+            return True
+
+        if i in cache:
+            return cache[i]
+
+        res = False
+
+        if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+            res = dfs(i + 2)
+        if i < len(nums) - 2:
+            if (nums[i] == nums[i + 1] == nums[i + 2]) or (nums[i] + 1 == nums[i + 1] == nums[i + 2] -1):
+                res = res or dfs(i + 3)
+
+        cache[i] = res
+        return res
+
+    return dfs(0)
 
 
 print(valid_partition([4,4,4,5,6]) == True)
