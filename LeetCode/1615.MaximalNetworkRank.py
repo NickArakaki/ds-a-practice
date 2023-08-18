9dbab6af4db8a977ed4f542c345b9f003adbe4a1
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-18 11:21:10 -0500

    add 16115

---
+++ LeetCode/1615.MaximalNetworkRank.py
@@ -0,0 +1,20 @@
+"""
+There is an infrastructure of n cities with some number of roads connecting these cities.
+Each roads[i] = [ai, bi] indicates that there is a bidirectional road between cities ai and bi.
+
+The network rank of two different cities is defined as the total number of directly connected
+roads to either city. If a road is directly connected to both cities, it is only counted once.
+
+The maximal network rank of the infrastructure is the maximum network rank of all pairs of different cities.
+
+Given the integer n and the array roads, return the maximal network rank of the entire infrastructure.
+"""
+
+
+def maximal_network_rank(n: int, roads: list[list[int]]) -> int:
+    pass
+
+
+print(maximal_network_rank(4, [[0, 1], [0, 3], [1, 2], [1, 3]]) == 4)
+print(maximal_network_rank(
+    5, [[0, 1], [0, 3], [1, 2], [1, 3], [2, 3], [2, 4]]) == 5)
