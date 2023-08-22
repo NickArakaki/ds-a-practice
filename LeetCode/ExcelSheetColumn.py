5bfeb0a4e32b2e9e708e423741beb6b93fe15c7d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-22 10:27:50 -0500

    add excel sheet col

--- LeetCode/ExcelSheetColumn.py
+++ LeetCode/ExcelSheetColumn.py
@@ -4,7 +4,14 @@ column title as it appears in an Excel sheet.
 """
 
 def convert_to_tile(columnNumber: int) -> str:
-    pass
+    res = []
+
+    while columnNumber > 0:
+        remainder = (columnNumber - 1) % 26
+        res.append(chr(ord("A") + remainder))
+        columnNumber = (columnNumber - 1) // 26
+
+    return "".join(res[::-1])
 
 print(convert_to_tile(1) == "A")
 print(convert_to_tile(28) == "AB")
