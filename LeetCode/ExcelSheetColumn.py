6244674a8a49028b6801b9a65f4e74485e596f94
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-21 21:47:26 -0500

    add excel sheet col

---
+++ LeetCode/ExcelSheetColumn.py
@@ -0,0 +1,11 @@
+"""
+Given an integer columnNumber, return its corresponding
+column title as it appears in an Excel sheet.
+"""
+
+def convert_to_tile(columnNumber: int) -> str:
+    pass
+
+print(convert_to_tile(1) == "A")
+print(convert_to_tile(28) == "AB")
+print(convert_to_tile(701) == "ZY")
