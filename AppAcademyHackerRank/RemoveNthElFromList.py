02b1834f201386b8aeb06b38824689710fce7405
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-23 15:08:18 -0500

    add remove nth node from end

--- AppAcademyHackerRank/RemoveNthElFromList.py
+++ AppAcademyHackerRank/RemoveNthElFromList.py
@@ -1,3 +1,22 @@
 """
 Given the head of a llist remove the nth node from the end
 """
+from Helpers import SinglyLinkedListNode
+
+def remove_nth_node(head, n):
+    dummy_node = SinglyLinkedListNode()
+    dummy_node.next = head
+
+    right_node = head
+    while n > 1:
+        right_node = right_node.next
+
+    prev_node = dummy_node
+    left_node = head
+    while right_node.next:
+        right_node = right_node.next
+        prev_node = prev_node.next
+        left_node = left_node.next
+
+    prev_node.next = left_node.next
+    return dummy_node.next
