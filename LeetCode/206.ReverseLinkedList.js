54082b3ae1e28bde6516700f28a24fcb30fc6d8d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-24 13:28:41 -0500

    add iterative js solution

--- LeetCode/206.ReverseLinkedList.js
+++ LeetCode/206.ReverseLinkedList.js
@@ -1,35 +1,41 @@
 // """
 // Given the head of a singly linked list, reverse the list, and return the reversed list.
 // """
-// # Recursive Solution
-// # def reverseList(head):
-// #     prev = None
 
-// #     def _helper(prev, head):
-// #         if head is None:
-// #             return prev
-
-// #         next_node = head.next
-// #         head.next = prev
-// #         return _helper(head, next_node)
-
-// #     return _helper(prev, head)
-
-// # Iterative Solution
-// # def revserSelis(head):
-// #     prev = None
-
-// #     curr_node = head
-// #     while curr_node:
-// #         next_node = curr_node.next
-// #         curr_node.next = prev
-// #         prev = curr_node
-// #         curr_node = next_node
-
-// #     return prev
-
-class Node {}
+class Node {
+  constructor(val, next) {
+    (this.val = val == undefined ? 0 : val),
+      (this.next = next == undefined ? null : next);
+  }
+}
 
 function reverseList(head) {
   let prev = null;
+  let curr = head;
+
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
 }
+
+let node4 = new Node(4);
+let node3 = new Node(3, node4);
+let node2 = new Node(2, node3);
+let node1 = new Node(1, node2);
+
+function printList(head) {
+  while (head) {
+    console.log(head);
+    head = head.next;
+  }
+}
+
+printList(node1);
+reverseList(node1);
+console.log("--------------------------");
+printList(node4);
