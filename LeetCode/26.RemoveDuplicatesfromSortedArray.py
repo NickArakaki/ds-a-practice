47424fde96975dda2ef44e991d90b6ec111a52f5
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-08-30 12:34:30 -0500

    add linear solution

--- LeetCode/26.RemoveDuplicatesfromSortedArray.py
+++ LeetCode/26.RemoveDuplicatesfromSortedArray.py
@@ -16,17 +16,28 @@ def remove_duplicates(nums: list[int]) -> int:
     seen_nums = set()
 
     # l and r pointers
+    l = r = 0
     # iterate thru nums with l and r pointers
+    while l < len(nums) and r < len(nums):
         # if nums[l] in seen_nums:
+        if nums[l] in seen_nums:
             # move r pointer to first number not in nums or until end of nums
-            # if r >= len(nums):
-                # break
-            # elif nums[r] not in seen_nums
+            while r < len(nums) and nums[r] in seen_nums:
+                r += 1
+
+            # if nums[r] not in seen_nums
                 # swap nums[l] and nums[r]
+            if r < len(nums) and nums[r] not in seen_nums:
+                nums[l], nums[r] = nums[r], nums[l]
+        seen_nums.add(nums[l])
+        l += 1
+        r += 1
         # add nums[l] to seen_nums
         # increment l and r pointers
-    #
     # return length of seen_nums set
+    return len(seen_nums)
 
-print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]) == [0,1,2,3,4])
-print(remove_duplicates([1,1,2]) == [1,2])
+print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]) == 5)
+print(remove_duplicates([1,1,2]) == 2)
+print(remove_duplicates([1,1,1]) == 1)
+print(remove_duplicates([1]) == 1)
