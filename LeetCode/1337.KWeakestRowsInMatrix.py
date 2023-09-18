120d182fd8f48d2621cc27e2dda576f53a1f860d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-18 12:16:42 -0500

    add initial solution

--- LeetCode/1337.KWeakestRowsInMatrix.py
+++ LeetCode/1337.KWeakestRowsInMatrix.py
@@ -10,26 +10,54 @@ A row i is weaker than a row j if one of the following is true:
 Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
 """
 
+from collections import defaultdict
+
 def k_weakest_rows(mat: list[list[int]], k: int) -> list[int]:
-    # helper func to get num 1s in row using modified bin search
-        # takes in list
-        # returns int (idx of first 0)
+    def _count_soldiers(row: list) -> int:
+        """
+            helper func to get num 1s in row using modified bin search
+            takes in list
+            returns int (idx of first 0)
+        """
         # init l and r pointers
+        l, r = 0, len(row)
         # while l < r
+        while l < r:
             # calc mid
+            mid = (l + r) // 2
+            el = row[mid]
             # if el at mid == 0 and el before mid is valid and not 0, we found the first 0
+            if (el == 0 and mid == 0) or (el == 0 and row[mid - 1] == 1):
+                return mid
             # elif el at mid == 0 and el before is also 0, search left half
+            elif el == 0 and row[mid - 1] == 0:
+                r = mid - 1
             # else search right
-        # if we we exit loop
+            else:
+                l = mid + 1
+        # if we we exit loop, it's all 1s
+        return len(row) + 1
 
     # use a dict to cache helper func results
         # key = num 1s
         # val = list of indices
+    cache = defaultdict(list)
+
+    # iterate thru matrix rows
+    for i, row in enumerate(mat):
+        num_soldiers = _count_soldiers(row)
+        cache[num_soldiers].append(i)
+
+    res = []
     # iterate thru keys (0 -> n)
-        # iterate thru list @ key
-            # add el to res list until len of res == k
-    # return res
-    pass
+    for i in range(len(mat[0])):
+        rows = cache[i]
+        for row in rows:
+            if len(res) == k:
+                return res
+            res.append(row)
+
+    return res
 
 
 print(k_weakest_rows([[1,1,0,0,0],
