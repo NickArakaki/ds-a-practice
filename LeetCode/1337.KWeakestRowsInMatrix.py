427bcea1087e2ccfca1e51cbeef58c9565b5e7c4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-18 13:02:19 -0500

    solution using min heap

--- LeetCode/1337.KWeakestRowsInMatrix.py
+++ LeetCode/1337.KWeakestRowsInMatrix.py
@@ -11,6 +11,54 @@ Return the indices of the k weakest rows in the matrix ordered from weakest to s
 """
 
 from collections import defaultdict
+from heapq import heapify, nsmallest
+
+# def k_weakest_rows(mat: list[list[int]], k: int) -> list[int]:
+#     def _count_soldiers(row: list) -> int:
+#         """
+#             helper func to get num 1s in row using modified bin search
+#             takes in list
+#             returns int (idx of first 0)
+#         """
+#         # init l and r pointers
+#         l, r = 0, len(row) - 1
+#         # while l < r
+#         while l <= r:
+#             # calc mid
+#             mid = (l + r) // 2
+#             el = row[mid]
+#             # if el at mid == 0 and el before mid is valid and not 0, we found the first 0
+#             if (el == 0 and mid == 0) or (el == 0 and row[mid - 1] == 1):
+#                 return mid
+#             # elif el at mid == 0 and el before is also 0, search left half
+#             elif el == 0 and row[mid - 1] == 0:
+#                 r = mid - 1
+#             # else search right
+#             elif el == 1:
+#                 l = mid + 1
+#         # if we we exit loop without returning, it's all 1s
+#         return len(row)
+
+#     # use a dict to cache helper func results
+#         # key = num 1s
+#         # val = list of indices
+#     cache = defaultdict(list)
+
+#     # iterate thru matrix rows
+#     for i, row in enumerate(mat):
+#         num_soldiers = _count_soldiers(row)
+#         cache[num_soldiers].append(i)
+
+#     res = []
+#     # iterate thru keys (0 -> n)
+#     for i in range(len(mat[0]) + 1):
+#         rows = cache[i]
+#         for row in rows:
+#             if len(res) == k:
+#                 return res
+#             res.append(row)
+#     return res
+
 
 def k_weakest_rows(mat: list[list[int]], k: int) -> list[int]:
     def _count_soldiers(row: list) -> int:
@@ -38,42 +86,26 @@ def k_weakest_rows(mat: list[list[int]], k: int) -> list[int]:
         # if we we exit loop without returning, it's all 1s
         return len(row)
 
-    # use a dict to cache helper func results
-        # key = num 1s
-        # val = list of indices
-    cache = defaultdict(list)
-
-    # iterate thru matrix rows
-    for i, row in enumerate(mat):
-        num_soldiers = _count_soldiers(row)
-        cache[num_soldiers].append(i)
-
-    res = []
-    # iterate thru keys (0 -> n)
-    for i in range(len(mat[0]) + 1):
-        rows = cache[i]
-        for row in rows:
-            if len(res) == k:
-                return res
-            res.append(row)
-    return res
+    queue = [(_count_soldiers(row), i) for i, row in enumerate(mat)]
+    heapify(queue)
+    return [i for _, i in nsmallest(k, queue)]
 
 
-# print(k_weakest_rows([
-#     [1,1,0,0,0],
-#     [1,1,1,1,0],
-#     [1,0,0,0,0],
-#     [1,1,0,0,0],
-#     [1,1,1,1,1]
-#  ], 3) == [2, 0, 3])
+print(k_weakest_rows([
+    [1,1,0,0,0],
+    [1,1,1,1,0],
+    [1,0,0,0,0],
+    [1,1,0,0,0],
+    [1,1,1,1,1]
+ ], 3) == [2, 0, 3])
 
-# print(k_weakest_rows([
-#     [1,0,0,0],
-#     [1,1,1,1],
-#     [1,0,0,0],
-#     [1,0,0,0]
-#  ], 2) == [0,2])
+print(k_weakest_rows([
+    [1,0,0,0],
+    [1,1,1,1],
+    [1,0,0,0],
+    [1,0,0,0]
+ ], 2) == [0,2])
 
-# print(k_weakest_rows([[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]], 1) == [0])
+print(k_weakest_rows([[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]], 1) == [0])
 
 print(k_weakest_rows([[1,0],[1,0],[1,0],[1,1]], 4) == [0,1,2,3])
