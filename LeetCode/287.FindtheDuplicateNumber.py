9dcd6ab68d436ef1276100a14235aab2d19cc376
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-19 12:24:52 -0500

    add floyds solution

--- LeetCode/287.FindtheDuplicateNumber.py
+++ LeetCode/287.FindtheDuplicateNumber.py
@@ -13,23 +13,40 @@ You must solve the problem without modifying the array nums and uses only consta
 #                 return cur_num
 #     return -1
 
+
+# def find_duplicate(nums: list[int]) -> int:
+#     length = len(nums)
+#     l = 1
+#     r = length - 1
+#     while l < r:
+#         mid = l + (r - l) // 2
+#         count = 0
+#         for i in range(length):
+#             if nums[i] <= mid:
+#                 count += 1
+
+#         if count <= mid:
+#             l = mid + 1
+#         else:
+#             r = mid
+#     return l
+
+
 def find_duplicate(nums: list[int]) -> int:
-    length = len(nums)
-    l = 1
-    r = length - 1
-    while l < r:
-        mid = l + (r - l) // 2
-        count = 0
-        for i in range(length):
-            if nums[i] <= mid:
-                count += 1
+    slow = fast = 0
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
 
-        if count <= mid:
-            l = mid + 1
-        else:
-            r = mid
-    return l
-
+    slow2 = 0
+    while True:
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if slow == slow2:
+            break
+    return slow
 
 print(find_duplicate([1,3,4,2,2]) == 2)
 print(find_duplicate([3,1,3,4,2]) == 3)
