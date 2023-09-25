f97c47e259fe5efc03fa7054fd9b0074007d8cdc
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-25 16:55:00 -0500

    add solution for request parser

--- HackerRank/RequestParser.py
+++ HackerRank/RequestParser.py
@@ -7,7 +7,61 @@ VALID, include a comma-separated string of parameters
 """
 
 def get_responses(valid_auth_tokens, requests):
-    pass
+    # Write your code here
+    # cast valid_auth_tokens list to set to improve look up time
+    valid_tokens = set(valid_auth_tokens)
+    # responses list
+    responses = []
+    # iterate through requests
+    # unpack into req method and url
+    for method, url in requests:
+        # has_valid_token = False
+        # has_valid_csrf = False if method == "POST" else True
+        has_valid_token = False
+        has_valid_csrf = False if method == "POST" else True
+
+        # get req_param_string by splitting req_parameters on "?"
+        req_param_str = url.split("?")
+        # get req_param list by splitting req_param_string on "&"
+        req_params = req_param_str[1].split("&")
+        # response_str = ""
+        response = ""
+        # iterate through req_param list
+        for param in req_params:
+            # param_key_value = split param on "="
+            key_val = param.split("=")
+            # if param[0] == "token"
+            if key_val[0] == "token":
+                # check if string after "=" is in valid_auth_tokens
+                # if True has_valid_token = True
+                has_valid_token = key_val[1] in valid_tokens
+            # if method == "POST" and param[0] == "csrf"
+            elif method == "POST" and key_val[0] == "csrf":
+                # check if string after "=" is in valid_auth_tokens
+                # if True has_valid_csrf = True
+                has_valid_csrf = is_valid_csrf(key_val[1])
+            # else
+            else:
+                response = f"{response},{','.join(key_val)}"
+                # join param_key_value on ","
+                # concat to response_str
+        # if has_valid_token and has_valid_csrf:
+        if has_valid_token and has_valid_csrf:
+            # responses_list.append(f"VALID,{response_str}")
+            responses.append(f"VALID{response}")
+        # else:
+        else:
+            # responses_list.append("INVALID")
+            responses.append("INVALID")
+
+    # return responses list
+    return responses
+
+
+def is_valid_csrf(token):
+    if len(token) < 8 or not token.isalnum() or token.lower() != token:
+        return False
+    return True
 
 
 print(get_responses(["ah37j2ha483u", "safh34ywb0p5", "ba34wyi8t902"], [
