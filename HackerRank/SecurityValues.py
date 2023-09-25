c1b6c715ec68e1f328f0fb434aac0b2c7676b88c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-25 16:55:32 -0500

    add sorting solution

--- HackerRank/SecurityValues.py
+++ HackerRank/SecurityValues.py
@@ -8,7 +8,18 @@ security values of adjacent characters.
 """
 
 def get_min_sum(security_values, msg):
-    pass
+    values = []
+    for char in msg:
+        index = ord(char.lower()) - ord("a")
+        values.append(security_values[index])
+    # sort list
+    values.sort()
+    diff_sum = 0
+    # iterate through list and get absolute val of each el and neighbor, add to sum
+    for i, val in enumerate(values[:-1:]):
+        diff_sum += abs(val - values[i + 1])
+    # return sum
+    return diff_sum
 
 
 print(get_min_sum([1,2,1,3,1,3,5,7,1,1,5,5,8,10,11,1,23,2,3,7,8,9,1,6,5,9], "afeb") == 2)
