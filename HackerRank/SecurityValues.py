da0ad7e26b1ed2adcfb0c5924e3203418d68430e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-26 10:42:27 -0500

    add linear solution

--- HackerRank/SecurityValues.py
+++ HackerRank/SecurityValues.py
@@ -23,20 +23,31 @@ from heapq import heappush, heappop
 #     return diff_sum
 
 
+# def get_min_sum(security_values, msg):
+#     values = []
+#     for char in msg:
+#         index = ord(char.lower()) - ord("a")
+#         heappush(values, security_values[index])
+
+#     diff_sum = 0
+#     prev = heappop(values)
+#     while values:
+#         val = heappop(values)
+#         diff_sum += abs(prev - val)
+#         prev = val
+
+#     return diff_sum
+
+
 def get_min_sum(security_values, msg):
-    values = []
-    for char in msg:
-        index = ord(char.lower()) - ord("a")
-        heappush(values, security_values[index])
+    mi, mx = float("inf"), 0
 
-    diff_sum = 0
-    prev = heappop(values)
-    while values:
-        val = heappop(values)
-        diff_sum += abs(prev - val)
-        prev = val
+    for c in msg:
+        val = security_values[ord(c) - ord("a")]
+        mi = min(mi, val)
+        mx = max(mx, val)
 
-    return diff_sum
+    return mx - mi
 
 
 print(get_min_sum([1,2,1,3,1,3,5,7,1,1,5,5,8,10,11,1,23,2,3,7,8,9,1,6,5,9], "afeb") == 2)
