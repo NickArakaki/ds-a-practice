df348eb8c9b6166a1cefe4709e9ebef0d89ea09d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-27 15:03:35 -0500

    add plan for longest palindrome substring

--- LeetCode/TODO/5.LongestPalidnromicSubstring.py
+++ LeetCode/TODO/5.LongestPalidnromicSubstring.py
@@ -1,9 +1,25 @@
 """
 Given a string s, return the longest
 palindromic substring in s.
+
+Trick is to check if palindrome inside-out rather that outside-in and account for even and odd len strs
 """
 
 def longest_palindrom(s: str) -> str:
+    # init res str
+    # iterate through each char of s
+        # first check for odd len palindrome
+        # init l and r pointers to be same position
+        # while l and r pointers are inbound and the val at l and r are equal
+            # move pointers outward
+            # if current len substring > max substring, update
+
+        # check if even len palindrom
+        # r points to next char in str
+        # while l and r are inbounds and chars at l and r are equal
+            # move pointers
+            # update accordingly
+        # return res str
     pass
 
 
