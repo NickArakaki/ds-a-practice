e130854d901a44c32bcdb55b942c24b00a17bd5e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-27 15:09:34 -0500

    add inside out solution for longest palindromic substring

--- LeetCode/TODO/5.LongestPalidnromicSubstring.py
+++ LeetCode/TODO/5.LongestPalidnromicSubstring.py
@@ -20,7 +20,25 @@ def longest_palindrom(s: str) -> str:
             # move pointers
             # update accordingly
         # return res str
-    pass
+    res = ""
+    for i, char in enumerate(s):
+        # odd
+        l = r = i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if r - l + 1 > len(res):
+                res = s[l:r + 1]
+            l -= 1
+            r += 1
+
+        # even
+        l,r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if r - l + 1 > len(res):
+                res = s[l:r+1]
+            l -= 1
+            r += 1
+
+    return res
 
 
 print(longest_palindrom("babad") == "bab")
