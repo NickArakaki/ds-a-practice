c40f0620b1dd2453ca9612b8060e486b8cbafea7
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-27 14:58:28 -0500

    add longest palindromic substring to backlog

---
+++ LeetCode/TODO/5.LongestPalidnromicSubstring.py
@@ -0,0 +1,11 @@
+"""
+Given a string s, return the longest
+palindromic substring in s.
+"""
+
+def longest_palindrom(s: str) -> str:
+    pass
+
+
+print(longest_palindrom("babad") == "bab")
+print(longest_palindrom("cbbd") == "bb")
