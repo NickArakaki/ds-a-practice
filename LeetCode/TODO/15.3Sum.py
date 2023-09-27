58670fb9616e5983746a2f51221bbf2ae3eba7de
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-27 13:40:28 -0500

    add 3 sum

--- LeetCode/TODO/15.3Sum.py
+++ LeetCode/TODO/15.3Sum.py
@@ -6,8 +6,36 @@ Notice that the solution set must not contain duplicate triplets.
 """
 
 def three_sum(nums: list[int]) -> list[list[int]]:
-    pass
+    # init a res array
+    res = []
+    # init seen set
+    seen = set()
+    # iterate through nums
+    for i, num in enumerate(nums):
+        if num not in seen:
+            seen.add(num)
+            # get difference between 0 and num
+            target = 0 - num
+            # pass remaining nums to two sum helper with difference as the target
+            combos = two_sum(nums[i:], target)
+            # iterate thru returned list
+            for combo in combos:
+                # add num to each sublist
+                combo.append(num)
+                # append sublist to res
+                res.append(combo)
+    # return res
+    return res
 
+def two_sum(nums: list[int], target: int) -> list[list[int]]:
+    # init a hashmap diff between num and target as key, val is num
+    # init res list
+    # iterate through nums
+        # calculate diff
+        # if diff in hashmap
+            #
+    # return res list
+    pass
 
 print(three_sum([-1,0,1,2,-1,-4]) == [[-1,-1,2],[-1,0,1]])
 print(three_sum([]) == [])
