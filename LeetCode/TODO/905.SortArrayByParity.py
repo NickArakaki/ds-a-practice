7080b11dca87b609ededdf73a1e2637e513f3a57
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-28 12:16:51 -0500

    add plan for sort by parity

--- LeetCode/TODO/905.SortArrayByParity.py
+++ LeetCode/TODO/905.SortArrayByParity.py
@@ -6,6 +6,12 @@ Return any array that satisfies this condition.
 """
 
 def sort_array_by_parity(nums: list[int]) -> list[int]:
+    # two pointers init to first el of list
+    # move l and r pointers to first odd num
+    # move right pointer while inbounds and num ar r is odd
+        # if num at r is even swap nums
+        # increment pointers
+    # return nums
     pass
 
 
