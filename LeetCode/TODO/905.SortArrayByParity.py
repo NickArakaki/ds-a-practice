b33718598b860640c1417740efa0d01a4858d34e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-28 12:28:24 -0500

    add linear time, constant space solution using two pointers

--- LeetCode/TODO/905.SortArrayByParity.py
+++ LeetCode/TODO/905.SortArrayByParity.py
@@ -7,13 +7,18 @@ Return any array that satisfies this condition.
 
 def sort_array_by_parity(nums: list[int]) -> list[int]:
     # two pointers init to first el of list
-    # move l and r pointers to first odd num
-    # move right pointer while inbounds and num ar r is odd
-        # if num at r is even swap nums
-        # increment pointers
-    # return nums
-    pass
+    l = 0
+    # move left pointer to find first odd number
+    while l < len(nums) and nums[l] % 2 == 0:
+        l += 1
+
+    for r in range(l + 1, len(nums)):
+        if nums[r] % 2 == 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+    return nums
 
 
 print(sort_array_by_parity([3,1,2,4]) == [2,4,3,1])
+print(sort_array_by_parity([3,4,1,2,3,4,6]) == [4,2,4,6,3,1,3])
 print(sort_array_by_parity([0]) == [0])
