120f81b2f7003f11f4d3a79bc366ba9244c8044b
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-28 12:11:34 -0500

    add sort array by parity to backlog

---
+++ LeetCode/TODO/905.SortArrayByParity.py
@@ -0,0 +1,13 @@
+"""
+Given an integer array nums, move all the even
+integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+"""
+
+def sort_array_by_parity(nums: list[int]) -> list[int]:
+    pass
+
+
+print(sort_array_by_parity([3,1,2,4]) == [2,4,3,1])
+print(sort_array_by_parity([0]) == [0])
