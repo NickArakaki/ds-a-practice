89a6c57a42ac91497989fa9d23a081a70b19cbfd
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-09-29 11:45:32 -0500

    update solution to account for out of bounds

--- LeetCode/Backlog/896.MonotonicArray.py
+++ LeetCode/Backlog/896.MonotonicArray.py
@@ -8,15 +8,12 @@ Given an integer array nums, return true if the given array is monotonic, or fal
 """
 
 def is_monotonic(nums: list[int]) -> bool:
-    # three pointers
-    # init l pointer to 0
     l = mid = 0
-    while mid < len(nums) and nums[mid] == nums[l]:
+    while mid < len(nums) - 1 and nums[mid] == nums[l]:
         mid += 1
     r = mid
-    # iterate while r < len nums
-    while r < len(nums):
-        while r < len(nums) and nums[mid] == nums[r]:
+    while r < len(nums) - 1:
+        while r < len(nums) - 1 and nums[mid] == nums[r]:
             r += 1
         if nums[l] > nums[mid] < nums[r] or nums[l] < nums[mid] > nums[r]:
             return False
@@ -25,7 +22,8 @@ def is_monotonic(nums: list[int]) -> bool:
     return True
 
 
-print(is_monotonic([1,2,2,3]) == True)
-print(is_monotonic([6,5,4,4]) == True)
-print(is_monotonic([1,3,2]) == False)
-print(is_monotonic([11,11,9,4,3,3,3,1,-1,-1,3,3,3,5,5,5]) == False)
+# print(is_monotonic([1,2,2,3]) == True)
+# print(is_monotonic([6,5,4,4]) == True)
+# print(is_monotonic([1,3,2]) == False)
+# print(is_monotonic([11,11,9,4,3,3,3,1,-1,-1,3,3,3,5,5,5]) == False)
+print(is_monotonic([5,5,5,5,5,5]) == True)
