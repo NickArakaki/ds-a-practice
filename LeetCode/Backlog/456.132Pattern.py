dccaf926cc41027368114bac66f8d334ad4f3431
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-02 14:38:37 -0500

    inital solution using stack

--- LeetCode/Backlog/456.132Pattern.py
+++ LeetCode/Backlog/456.132Pattern.py
@@ -6,17 +6,21 @@ Return true if there is a 132 pattern in nums, otherwise, return false.
 """
 
 def find_132_pattern(nums: list[int]) -> bool:
-    # if len nums < 3 return false
-    # use a stack to keep track of max and mins
-    # init stack with tup (num[0], num[0]) -> i, j
+    if len(nums) < 3: return False
+    stack = [(nums[0], nums[0])] # i, j
     # iterate thru nums (start from 1 -> end)
-        # pop from stack while cur num > max seen prior
-        # check if nums[i] < nums[k] < nums[j]
-        # add cur num, and min prev to stack
+    for num in nums[1:]:
+        prior_min = stack[-1][0]
+        while stack and num > stack[-1][1]:
+            min, max = stack.pop()
+            prior_min = min
+        if stack and stack[-1][0] < num < stack[-1][1]:
+            return True
+        stack.append((prior_min, num))
     # return False
-    pass
+    return False
 
-# print(find_132_pattern([1,2,3,4]) == False)
-# print(find_132_pattern([3,1,4,2]) == True)
-# print(find_132_pattern([-1,3,2,0]) == True)
+print(find_132_pattern([1,2,3,4]) == False)
+print(find_132_pattern([3,1,4,2]) == True)
+print(find_132_pattern([-1,3,2,0]) == True)
 print(find_132_pattern([3,5,0,3,4]) == True)
