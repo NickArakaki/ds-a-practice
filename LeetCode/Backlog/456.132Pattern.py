bcb788e86bc03e5448ac15280cce1d3b5e801bbe
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-02 12:58:49 -0500

    add 132 pattern to backlog

---
+++ LeetCode/Backlog/456.132Pattern.py
@@ -0,0 +1,14 @@
+"""
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i],
+nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+"""
+
+def find_132_pattern(nums: list[int]) -> bool:
+    pass
+
+
+print(find_132_pattern([1,2,3,4]) == False)
+print(find_132_pattern([3,1,4,2]) == True)
+print(find_132_pattern([-1,3,2,0]) == True)
