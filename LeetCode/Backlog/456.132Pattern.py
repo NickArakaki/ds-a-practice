77e0f630346c26c8f2972a4ceca378431a432403
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-02 13:08:02 -0500

    found edge case

--- LeetCode/Backlog/456.132Pattern.py
+++ LeetCode/Backlog/456.132Pattern.py
@@ -8,10 +8,11 @@ Return true if there is a 132 pattern in nums, otherwise, return false.
 def find_132_pattern(nums: list[int]) -> bool:
     if len(nums) < 3: return False
     for i in range(1, len(nums) - 1):
-        if nums[i - 1] < nums[i + 1] < nums[i]: return True
+        if nums[i] < nums[i + 1] < nums[i - 1]: return True
     return False
 
 
-print(find_132_pattern([1,2,3,4]) == False)
-print(find_132_pattern([3,1,4,2]) == True)
-print(find_132_pattern([-1,3,2,0]) == True)
+# print(find_132_pattern([1,2,3,4]) == False)
+# print(find_132_pattern([3,1,4,2]) == True)
+# print(find_132_pattern([-1,3,2,0]) == True)
+print(find_132_pattern([3,5,0,3,4]) == True)
