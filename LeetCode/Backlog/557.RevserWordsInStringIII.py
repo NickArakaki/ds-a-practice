71d8247a33256cfbac3ea760cd470bf8b1281ea1
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-02 12:44:32 -0500

    add reverse words in string 3 to back log

---
+++ LeetCode/Backlog/557.RevserWordsInStringIII.py
@@ -0,0 +1,11 @@
+"""
+Given a string s, reverse the order of characters in each word within a sentence while still
+preserving whitespace and initial word order.
+"""
+
+def reverse_words(s: str) -> str:
+    pass
+
+
+print(reverse_words("Let's take LeetCode contest") == "s'teL ekat edoCteeL tsetnoc")
+print(reverse_words("God Ding") == "doG gniD")
