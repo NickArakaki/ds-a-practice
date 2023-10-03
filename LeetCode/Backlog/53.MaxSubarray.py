0d75c35f71967534d2736b1ecd15c1b8de3cac2c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:51:44 -0500

    add initial linear solution

--- LeetCode/Backlog/53.MaxSubarray.py
+++ LeetCode/Backlog/53.MaxSubarray.py
@@ -6,12 +6,19 @@ subarray with the largest sum, and return its sum.
 def max_subarray(nums: list[int]) -> int:
     # track max sum
     # track cur sum
+    max_sum = cur_sum = 0
     # iterate thru nums
+    for num in nums:
+        if cur_sum <= 0:
+            cur_sum = 0
+        cur_sum += num
+        max_sum = max(max_sum, cur_sum)
         # if prefix sum <= 0:
             # set cur_sum to 0, essentially starting from this idx
         # add cur num to cur sum
         # set max sum to max of max sum/cur sum
     # return max sum
+    return max_sum
     pass
 
 
