ed4dfa4baa2377014cb8342dcdb0515ff779964c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:56:53 -0500

    account for edge case where first el is neg

--- LeetCode/Completed/Arrays/53.MaxSubarray.py
+++ LeetCode/Completed/Arrays/53.MaxSubarray.py
@@ -4,7 +4,8 @@ subarray with the largest sum, and return its sum.
 """
 
 def max_subarray(nums: list[int]) -> int:
-    max_sum = cur_sum = 0
+    max_sum = float("-inf")
+    cur_sum = 0
     for num in nums:
         if cur_sum < 0:
             cur_sum = 0
@@ -15,4 +16,5 @@ def max_subarray(nums: list[int]) -> int:
 
 print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]) == 6)
 print(max_subarray([1]) == 1)
+print(max_subarray([-1]) == -1)
 print(max_subarray([5,4,-1,7,8]) == 23)
