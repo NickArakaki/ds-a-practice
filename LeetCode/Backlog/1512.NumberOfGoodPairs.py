0517b75b93f90dd6a3ee2d6312ac704dc862011f
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:10:50 -0500

    add num good pairs linear solution

--- LeetCode/Backlog/1512.NumberOfGoodPairs.py
+++ LeetCode/Backlog/1512.NumberOfGoodPairs.py
@@ -3,13 +3,16 @@ Given an array of integers nums, return the number of good pairs.
 
 A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 """
+from collections import Counter
 
 def num_identical_pairs(nums: list[int]) -> int:
-    # count each el of nums
-    # iterate thru keys of count
-        # calculate num pairs for each count n(n-1) / 2
-    # return num pairs
-    pass
+    count = Counter(nums)
+    pairs = 0
+    for num in count:
+        num_count = count[num]
+        num_pairs = num_count * (num_count - 1) // 2
+        pairs += num_pairs
+    return pairs
 
 
 print(num_identical_pairs([1,2,3,1,1,3]) == 4) # (0,3), (0,4), (3,4), (2,5)
