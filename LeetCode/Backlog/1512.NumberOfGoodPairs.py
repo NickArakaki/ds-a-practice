4c44c512caeeb6427149036acfb9d6cff4387075
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:16:46 -0500

    refactor to be more readable

--- LeetCode/Backlog/1512.NumberOfGoodPairs.py
+++ LeetCode/Backlog/1512.NumberOfGoodPairs.py
@@ -6,12 +6,10 @@ A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 from collections import Counter
 
 def num_identical_pairs(nums: list[int]) -> int:
-    count = Counter(nums)
+    num_count = Counter(nums)
     pairs = 0
-    for num in count:
-        num_count = count[num]
-        num_pairs = num_count * (num_count - 1) // 2
-        pairs += num_pairs
+    for count in num_count.values():
+        pairs += count * (count - 1) // 2
     return pairs
 
 
