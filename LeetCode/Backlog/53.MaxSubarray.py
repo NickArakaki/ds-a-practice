ae8c17097f34c012b8033dc751f7e0cff5d42087
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:29:44 -0500

    add max sub array to backlog

---
+++ LeetCode/Backlog/53.MaxSubarray.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums, find the
+subarray with the largest sum, and return its sum.
+"""
+
+def max_subarray(nums: list[int]) -> int:
+    pass
+
+
+print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]) == 6)
+print(max_subarray([1]) == 1)
+print(max_subarray([5,4,-1,7,8]) == 23)
