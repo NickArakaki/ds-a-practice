e5fc053f42216623d4a6c27b5aa26980e04035f6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 12:49:39 -0500

    add initial linear plan

--- LeetCode/Backlog/53.MaxSubarray.py
+++ LeetCode/Backlog/53.MaxSubarray.py
@@ -4,6 +4,14 @@ subarray with the largest sum, and return its sum.
 """
 
 def max_subarray(nums: list[int]) -> int:
+    # track max sum
+    # track cur sum
+    # iterate thru nums
+        # if prefix sum <= 0:
+            # set cur_sum to 0, essentially starting from this idx
+        # add cur num to cur sum
+        # set max sum to max of max sum/cur sum
+    # return max sum
     pass
 
 
