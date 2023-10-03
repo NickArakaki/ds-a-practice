0a0ff75ec50e20893ff283f81193918d46b30605
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-03 11:37:37 -0500

    add num identical pairs to backlog

---
+++ LeetCode/Backlog/1512.NumberOfGoodPairs.py
@@ -0,0 +1,13 @@
+"""
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+"""
+
+def num_identical_pairs(nums: list[int]) -> int:
+    pass
+
+
+print(num_identical_pairs([1,2,3,1,1,3]) == 4) # (0,3), (0,4), (3,4), (2,5)
+print(num_identical_pairs([1,1,1,1]) == 6) # each pair in the array
+print(num_identical_pairs([1,2,3]) == 0) # no identical pairs
