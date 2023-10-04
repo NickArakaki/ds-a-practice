c3b7e90d56fadc7a716aa598057ee72c6be86f07
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-04 16:15:28 -0500

    add ibm first hackerrank question

---
+++ HackerRank/IBMQuestion1.py
@@ -0,0 +1,13 @@
+"""
+An analyst is analyzing a stock over a period of n days. The price of the stock on the ith day is price[i],
+and the profit obtained is denoted by profit[i]. The analyst wants to pick a triplet of days (i,j,k) such that
+(i < j < k) adn price[i] < price[j] < price[k] in such a way that the total profit, i.e. profit[i] + profit[j] + profit[k]
+is maximized.
+Find the maximum total profit possible, if there is no valid triplet, return -1
+"""
+
+def max_profit(price, profit):
+    pass
+
+
+print(max_profit([1,5,3,4,6], [2,3,4,5,6]) == 15)
