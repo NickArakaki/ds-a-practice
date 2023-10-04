4eae42340537845d4869f3fcfcb81812a7f645d4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-04 11:33:04 -0500

    add design hashmap to backlog

---
+++ LeetCode/Backlog/706.DesignHashMap.py
@@ -0,0 +1,37 @@
+"""
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+
+void put(int key, int value) inserts a (key, value) pair into the HashMap.
+If the key already exists in the map, update the corresponding value.
+
+int get(int key) returns the value to which the specified key is mapped, or -1
+if this map contains no mapping for the key.
+
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+"""
+
+class MyHashMap:
+    def __init__(self):
+        pass
+
+
+    def put(self, key:int, value:int) -> None:
+        pass
+
+
+    def get(self, key: int) -> int:
+        pass
+
+
+    def remove(self, key: int) -> None:
+        pass
+
+
+obj = MyHashMap()
+obj.put(1,2)
+param_2 = obj.get(1)
+obj.remove(1)
