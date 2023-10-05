f35896f2a4deaf1fc98871abe6c818e9265c43c3
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-05 12:02:02 -0500

    use a list comprehension to clean up second pass

--- LeetCode/Backlog/229.MajorityElementII.py
+++ LeetCode/Backlog/229.MajorityElementII.py
@@ -5,7 +5,8 @@ Given an integer array of size n, find all elements that appear more than ⌊ n/
 def majority_element(nums: list[int]) -> list[int]:
     # Boyer Moore Majority Voting Algorithm
     # init counter and tracker for 2 nums, (there can only be at most 2 el that occur more than n/3 times in any array of any len)
-    num_1, num_2, counter_1, counter_2 = 0, 0, 0, 0
+    num_1 = num_2 = float("inf")
+    counter_1 = counter_2 = 0
 
     for num in nums:
         if num == num_1:
@@ -23,21 +24,22 @@ def majority_element(nums: list[int]) -> list[int]:
             counter_2 -= 1
 
     # set num1 counter and num2 counter to 0
-    counter_1 = counter_2 = 0
-    for num in nums:
-        if num == num_1:
-            counter_1 += 1
-        elif num == num_2:
-            counter_2 += 1
+    # counter_1 = counter_2 = 0
+    # for num in nums:
+    #     if num == num_1:
+    #         counter_1 += 1
+    #     elif num == num_2:
+    #         counter_2 += 1
 
-    res = []
-    n = len(nums)
-    if counter_1 > (n / 3):
-        res.append(num_1)
-    if counter_2 > (n / 3):
-        res.append(num_2)
+    # res = []
+    # n = len(nums)
+    # if counter_1 > (n / 3):
+    #     res.append(num_1)
+    # if counter_2 > (n / 3):
+    #     res.append(num_2)
 
-    return res
+    # return res
+    return [num for num in (num_1, num_2) if nums.count(num) > len(nums) / 3]
 
 print(majority_element([3,2,3]) == [3])
 print(majority_element([1]) == [1])
