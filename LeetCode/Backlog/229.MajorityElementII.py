14c3de23be086531b5549e1dc9a2dd9cca9eaf44
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-05 11:23:18 -0500

    add majority element

---
+++ LeetCode/Backlog/229.MajorityElementII.py
@@ -0,0 +1,11 @@
+"""
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+"""
+
+def majority_element(nums: list[int]) -> list[int]:
+    pass
+
+
+print(majority_element([3,2,3]) == [3])
+print(majority_element([1]) == [1])
+print(majority_element([1,2]) == [1,2])
