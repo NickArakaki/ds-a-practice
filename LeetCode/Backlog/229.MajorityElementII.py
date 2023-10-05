f4c8b4ad2dd53b3f1163bb5d2e5423f4cba5a1c1
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-05 11:53:23 -0500

    add linear time, constant space boyer moore voting algo solution

--- LeetCode/Backlog/229.MajorityElementII.py
+++ LeetCode/Backlog/229.MajorityElementII.py
@@ -5,34 +5,39 @@ Given an integer array of size n, find all elements that appear more than ⌊ n/
 def majority_element(nums: list[int]) -> list[int]:
     # Boyer Moore Majority Voting Algorithm
     # init counter and tracker for 2 nums, (there can only be at most 2 el that occur more than n/3 times in any array of any len)
-    # iterate thru nums
-        # if num == num1:
-            # increment num_1 counter
-        # elif num == num2
-            # increment num_2 counter
-        # elif num1 counter == 0:
-            # num1 = num
-        # elif num2 counter == 0:
-            # num2 = num
-        # else
-            # decrement num1 counter and num2 counter
+    num_1, num_2, counter_1, counter_2 = 0, 0, 0, 0
+
+    for num in nums:
+        if num == num_1:
+            counter_1 += 1
+        elif num == num_2:
+            counter_2 += 1
+        elif counter_1 == 0:
+            num_1 = num
+            counter_1 += 1
+        elif counter_2 == 0:
+            num_2 = num
+            counter_2 += 1
+        else:
+            counter_1 -= 1
+            counter_2 -= 1
 
     # set num1 counter and num2 counter to 0
-    # second pass through nums to count num1 and num2
-        # if num == num1:
-            # increment num1 counter
-        # elif num == num2:
-            # increment num2 counter
+    counter_1 = counter_2 = 0
+    for num in nums:
+        if num == num_1:
+            counter_1 += 1
+        elif num == num_2:
+            counter_2 += 1
 
-    # init res list
-    # if num1 counter == n // 3:
-        # add num1 to res
-    # if num2 counter == n // 3:
-        # add num2 to res
+    res = []
+    n = len(nums)
+    if counter_1 > (n / 3):
+        res.append(num_1)
+    if counter_2 > (n / 3):
+        res.append(num_2)
 
-    # return res
-    pass
-
+    return res
 
 print(majority_element([3,2,3]) == [3])
 print(majority_element([1]) == [1])
