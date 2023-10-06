4fc0167539934b9aabda06ade426b2cc7f2173f3
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-06 15:45:04 -0500

    add integer break to backlog

---
+++ LeetCode/Backlog/343.IntegerBreak.py
@@ -0,0 +1,12 @@
+"""
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+"""
+
+def integer_break(n: int) -> int:
+    pass
+
+
+print(integer_break(2) == 1)
+print(integer_break(10) == 36)
