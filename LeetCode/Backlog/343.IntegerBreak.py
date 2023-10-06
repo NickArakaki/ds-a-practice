c248e421e5872335719c2bfdcfd6f4dda8f2b989
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-06 15:53:23 -0500

    add initial recursive solution

--- LeetCode/Backlog/343.IntegerBreak.py
+++ LeetCode/Backlog/343.IntegerBreak.py
@@ -7,14 +7,23 @@ Return the maximum product you can get.
 def integer_break(n: int) -> int:
     # recursive solution
     # dfs
-        # base case: if num == 1 return 1
-        # initial val = 0 if current num == n else n because we need to break up n into at least 2 nums
-        # iterate thru range from 1 to num, non-inclusive
-            # get the product of dfs(i) * dfs(num - i)
-            # set val to the max of current val and the product
-        # return max val
-    # call dfs(n)
-    pass
+    # base case: if num == 1 return 1
+    # initial val = 0 if current num == n else n because we need to break up n into at least 2 nums
+    # iterate thru range from 1 to num, non-inclusive
+        # get the product of dfs(i) * dfs(num - i)
+        # set val to the max of current val and the product
+    # return max val
+# call dfs(n)
+    def _dfs(num) -> int:
+        if num == 1:
+            return 1
+
+        val = 0 if num == n else num
+        for i in range(1, num):
+            val = max(val, (_dfs(i) * _dfs(num - i)))
+        return val
+
+    return _dfs(n)
 
 
 print(integer_break(2) == 1)
