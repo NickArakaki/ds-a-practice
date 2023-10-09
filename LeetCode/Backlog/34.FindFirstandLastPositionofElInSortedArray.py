c919ecec1bb60f0713fc49eee718e3dd9bf3f2b6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-09 11:42:36 -0500

    add pseudocode plan

--- LeetCode/Backlog/34.FindFirstandLastPositionofElInSortedArray.py
+++ LeetCode/Backlog/34.FindFirstandLastPositionofElInSortedArray.py
@@ -9,9 +9,25 @@
 
 
 def search_range(nums: list[int], target: int) -> list[int]:
-    target_range = [-1, -1]
+    # start = end = -1
+    # two bin search
+    # init l and r pointers
+    # first for start
+    # iterate while l <= r
+        # calculate mid
+        # if mid == 0 or nums[mid - 1] < target and nums[mid] == target, found start
+        # elif nums[mid] > target: search right
+        # else search left
 
-    return target_range
+    # second for end
+    # set l pointer to start if start >= -1 else 0, r pointer to end of nums
+    # while l <= r:
+        # calculate mid
+        # if mid == len(nums) - 1 or nums[mid + 1] > target and nums[mid] == target, found end
+        # elif nums[mid] < target: search left
+        # else: search right
+
+    # return [start, end]
 
 
 
