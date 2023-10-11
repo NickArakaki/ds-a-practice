1085ab726bb45d73eba537305654fe292fb36cfe
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-11 11:33:14 -0500

    add full bloom problem to backlog

---
+++ LeetCode/Backlog/2251.NumberOfFlowersInFullBloom.py
@@ -0,0 +1,17 @@
+"""
+You are given a 0-indexed 2D integer array flowers, where flowers[i] = [starti, endi]
+means the ith flower will be in full bloom from starti to endi (inclusive).
+You are also given a 0-indexed integer array people of size n,
+where people[i] is the time that the ith person will arrive to see the flowers.
+
+Return an integer array answer of size n, where answer[i] is the number of
+flowers that are in full bloom when the ith person arrives.
+"""
+
+
+def full_bloom_flowers(flowers: list[list[int]], people: list[int]) -> list[int]:
+    pass
+
+
+print(full_bloom_flowers([[1,6],[3,7],[9,12],[4,13]], [2,3,7,11]) == [1,2,2,2])
+print(full_bloom_flowers([[1,10],[3,3]], [3,3,2]) == [2,2,1])
