d882d72084de13bb8be998bc41d185fd4bc3035b
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-13 11:19:04 -0500

    add min cost to climb stairs

---
+++ LeetCode/Backlog/746.MinCostClimbingStairs.py
@@ -0,0 +1,15 @@
+"""
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase.
+Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+"""
+
+def min_cost_climbing_stairs(cost: list[int]) -> int:
+    pass
+
+
+print(min_cost_climbing_stairs([10,15,20]) == 15)
+print(min_cost_climbing_stairs([1,100,1,1,1,100,1,1,100,1]) == 6)
