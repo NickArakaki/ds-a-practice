a0bd93d73bd3b742adcdbde9376aee74ba427950
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-14 18:29:06 -0500

    hacker rank solved

---
+++ HackerRank/MatrixEngergyTraversal.py
@@ -0,0 +1,40 @@
+"""
+Given a 4x4 matrix mat, the initial energy is 100. The task is to reach the
+last row of the matrix with the max possible energy left.
+
+The mat can be traversed in the following way:
+    1. Start with any cell in the first row
+    2. In each move, traverse the cell(i, j) of the ith row and jth col to
+        any existing cell out of (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
+
+After stepping on a cell (i, j), energy decreases by mat[i][j] units. Find the max possible
+energy left at the end of the traversal
+
+Note: final energy can be negative
+
+
+"""
+
+def maxEnergy(mat: list[list[int]]) -> int:
+    # Write your code here
+    dp = mat[-1]
+    for i in range(len(mat) - 2, -1, -1):
+        row = mat[i]
+        cur_cost = []
+
+        for j, val in enumerate(row):
+            l = r = float("inf")
+            c = dp[j]
+
+            if j > 0:
+                l = dp[j - 1]
+            if j < len(row) - 1:
+                r = dp[j + 1]
+            cur_cost.append(val + min(l,r,c))
+
+        dp = cur_cost
+
+    return 100 - min(dp)
+
+
+print(maxEnergy([[10,20,30,40], [60,50,20,80], [10,10,10,10], [60,50,60,50]]) == 0)
--- LeetCode/Completed/Arrays/746.MinCostClimbingStairs.py
+++ LeetCode/Completed/Arrays/746.MinCostClimbingStairs.py
@@ -8,12 +8,6 @@ Return the minimum cost to reach the top of the floor.
 """
 
 def min_cost_climbing_stairs(cost: list[int]) -> int:
-    # use a dp list to calculate the min cost to climb to that position
-        # init list [0, 0] -> no initial cost
-        # iterate through 2 -> end of list
-        # min cost to reach current stair = min(cost[-1] + total_cost[-1], cost[-2] + total_cost[-2])
-        # add to total_cost[i]
-    # return the min(total_cost[-1], total_cost[-2])
     dp = [0,0]
     for i in range(2, len(cost) + 1):
         dp.append(min(cost[i - 1] + dp[-1], cost[i - 2] + dp[-2]))
