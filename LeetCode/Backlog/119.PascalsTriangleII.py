397c653f7839c26b79ffd2b71272381cb7ad7d6d
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-16 12:01:41 -0500

    include linear time solution

--- LeetCode/Backlog/119.PascalsTriangleII.py
+++ LeetCode/Backlog/119.PascalsTriangleII.py
@@ -4,16 +4,24 @@ Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 """
 
+# def get_row(rowIndex: int) -> list[int]:
+#     prev = cur = [1]
+#     while len(prev) < rowIndex + 1:
+#         for i in range(len(prev) - 1):
+#             new_val = prev[i] + prev[i + 1]
+#             cur.append(new_val)
+#         cur.append(1)
+#         prev = cur
+#         cur = [1]
+#     return prev
+
+
 def get_row(rowIndex: int) -> list[int]:
-    prev = cur = [1]
-    while len(prev) < rowIndex + 1:
-        for i in range(len(prev) - 1):
-            new_val = prev[i] + prev[i + 1]
-            cur.append(new_val)
-        cur.append(1)
-        prev = cur
-        cur = [1]
-    return prev
+    res = [1]
+    for i in range(1, rowIndex + 1):
+        next_val = res[i - 1] * (rowIndex - i + 1) // i
+        res.append(next_val)
+    return res
 
 
 
