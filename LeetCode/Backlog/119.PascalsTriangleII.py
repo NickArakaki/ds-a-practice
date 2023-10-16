54b39ad2a1224aa514dda4246b2f9e7ef2989408
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-16 11:51:59 -0500

    use dp n^2 solution

--- LeetCode/Backlog/119.PascalsTriangleII.py
+++ LeetCode/Backlog/119.PascalsTriangleII.py
@@ -5,7 +5,15 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 """
 
 def get_row(rowIndex: int) -> list[int]:
-    pass
+    prev = cur = [1]
+    while len(prev) < rowIndex + 1:
+        for i in range(len(prev) - 1):
+            new_val = prev[i] + prev[i + 1]
+            cur.append(new_val)
+        cur.append(1)
+        prev = cur
+        cur = [1]
+    return prev
 
 
 
