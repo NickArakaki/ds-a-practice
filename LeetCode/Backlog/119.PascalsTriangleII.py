0d72c996a7179d90d1741c3ccc12c7890c391a7e
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-16 11:49:33 -0500

    add pascals triangle ii

---
+++ LeetCode/Backlog/119.PascalsTriangleII.py
@@ -0,0 +1,14 @@
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+"""
+
+def get_row(rowIndex: int) -> list[int]:
+    pass
+
+
+
+print(get_row(3) == [1,3,3,1])
+print(get_row(0) == [1])
+print(get_row(1) == [1,1])
