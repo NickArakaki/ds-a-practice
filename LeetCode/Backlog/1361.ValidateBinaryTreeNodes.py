ea9d02c445b639e74ffa5161c95b4796e20736fa
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-17 14:39:34 -0500

    add working solution

--- LeetCode/Backlog/1361.ValidateBinaryTreeNodes.py
+++ LeetCode/Backlog/1361.ValidateBinaryTreeNodes.py
@@ -21,16 +21,17 @@ def is_valid_binary_tree(root, leftChild, rightChild):
         l_child = leftChild[cur_node]
         r_child = rightChild[cur_node]
 
-        if l_child in visited or r_child in visited:
-            return False
-
-        if l_child > -1:
+        if l_child != -1:
+            if l_child in visited:
+                return False
             visited.add(l_child)
             queue.append(l_child)
 
-        if r_child > -1:
-            visited.add(l_child)
-            queue.append(l_child)
+        if r_child != -1:
+            if r_child in visited:
+                return False
+            visited.add(r_child)
+            queue.append(r_child)
 
     return True if len(visited) == len(leftChild) else False
 
@@ -57,9 +58,56 @@ def validate_binary_tree_nodes(n: int, leftChild: list[int], rightChild: list[in
     return is_valid_binary_tree(cur_root, leftChild, rightChild)
 
 
+# def is_valid_bin_tree(root: int, left_children: list[int], right_children: list[int]) -> bool:
+#         queue = deque()
+#         queue.append(root)
 
+#         visited = set()
+#         visited.add(root)
+
+#         while queue:
+#             cur_node = queue.popleft()
+#             l_child = left_children[cur_node]
+#             r_child = right_children[cur_node]
+
+#             if l_child != -1:
+#                 if l_child in visited:
+#                     return False
+#                 queue.append(l_child)
+#                 visited.add(l_child)
+
+#             if r_child != -1:
+#                 if r_child in visited:
+#                     return False
+#                 queue.append(r_child)
+#                 visited.add(r_child)
+
+#         return True if len(visited) == len(left_children) else False
+
+
+# def validate_binary_tree_nodes(n: int, leftChild: list[int], rightChild: list[int]) -> bool:
+#     # track which nodes have parents
+#     has_parent_list = [False] * n
+#     for node in range(n):
+#         l_node = leftChild[node]
+#         r_node = rightChild[node]
+
+#         if l_node > -1:
+#             has_parent_list[l_node] = True
+#         if r_node > -1:
+#             has_parent_list[r_node] = True
+
+#     root = -1
+#     for node, has_parent in enumerate(has_parent_list):
+#         if not has_parent:
+#             if root == -1:
+#                 root = node
+#             else:
+#                 return False
+
+#     return is_valid_bin_tree(root, leftChild, rightChild)
 
 print(validate_binary_tree_nodes(4, [1,-1,3,-1], [2,-1,-1,-1]) == True)
 print(validate_binary_tree_nodes(4, [1,-1,3,-1], [2,3,-1,-1]) == False)
-print(validate_binary_tree_nodes(n = 2, leftChild = [1,0], rightChild = [-1,-1]) == False)
+print(validate_binary_tree_nodes(2, [1,0], [-1, -1]) == False)
 print(validate_binary_tree_nodes(4, [3,-1,1,-1], [-1,-1,0,-1]) == True)
