a4938df441b14fcae3bde74bc9b192be218064e4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-17 12:58:50 -0500

    solution builds tree and returns false if ever not binary tree

--- LeetCode/Backlog/1361.ValidateBinaryTreeNodes.py
+++ LeetCode/Backlog/1361.ValidateBinaryTreeNodes.py
@@ -9,18 +9,43 @@ Note that the nodes have no values and that we only use the node numbers in this
 
 """
 
+# def validate_binary_tree_nodes(n: int, leftChild: list[int], rightChild: list[int]) -> bool:
+#     # create a set of nodes, init with 0th node
+#     node_set = set()
+#     node_set.add(0)
+#     for parent in range(n):
+#         l_child = leftChild[parent]
+#         r_child = rightChild[parent]
+#         if (parent not in node_set) or (l_child in node_set) or (r_child in node_set):
+#             return False
+#         if l_child > 0: node_set.add(l_child)
+#         if r_child > 0: node_set.add(r_child)
+#     return True
+
 def validate_binary_tree_nodes(n: int, leftChild: list[int], rightChild: list[int]) -> bool:
-    # create a set of nodes, init with 0th node
-    node_set = set()
-    node_set.add(0)
-    for parent in range(n):
-        l_child = leftChild[parent]
-        r_child = rightChild[parent]
-        if (parent not in node_set) or (l_child in node_set) or (r_child in node_set):
-            return False
-        if l_child > 0: node_set.add(l_child)
-        if r_child > 0: node_set.add(r_child)
-    return True
+    # init hashmap {node: [parent_set, children_set]} => { 0: { parent: None, children: set() }}
+
+    # iterate through list of nodes
+        # if node not in map there is a disconnect return false
+        # if l_child or r_child == cur_node return False
+
+        # if l_child not in hashmap
+            # hashmap[l_child] = { parent: cur_node, childrent: set() }
+        # else
+            # if hashmap[l_child].parent is not None:
+                # return False
+            # else hashmap[l_child] = cur_node
+
+        # if r_child not in hashmap
+            # hashmap[r_child] = { parent: cur_node, childrent: set() }
+        # else
+            # if hashmap[r_child].parent is not None:
+                # return False
+            # else hashmap[r_child] = cur_node
+
+    # return true
+
+
 
 # print(validate_binary_tree_nodes(4, [1,-1,3,-1], [2,-1,-1,-1]) == True)
 # print(validate_binary_tree_nodes(4, [1,-1,3,-1], [2,3,-1,-1]) == False)
