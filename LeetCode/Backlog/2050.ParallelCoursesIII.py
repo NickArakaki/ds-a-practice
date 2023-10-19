3c41312216fd358ae22d54d9d07748c8fc9dc86f
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-18 20:31:11 -0500

    working solution

--- LeetCode/Backlog/2050.ParallelCoursesIII.py
+++ LeetCode/Backlog/2050.ParallelCoursesIII.py
@@ -14,6 +14,7 @@ Return the minimum number of months needed to complete all the courses.
 Note: The test cases are generated such that it is possible to complete every course
 (i.e., the graph is a directed acyclic graph).
 """
+from collections import defaultdict
 
 def minimum_time(n: int, relations: list[list[int]], time: list[int]) -> int:
     # calculate time needed to complete each course
@@ -33,10 +34,43 @@ def minimum_time(n: int, relations: list[list[int]], time: list[int]) -> int:
             # cache[course] = cost
 
     # list end nodes
-    # iterate over end nodes, key into cache and add val to res
+    # iterate over end nodes, key into cache and get max of the vals
     # return res
-    pass
+    prereqs = defaultdict(list)
+    final_courses = set()
+    for prereq, course in relations:
+        prereqs[course].append(prereq)
+        final_courses.add(course)
+        final_courses.discard(prereq)
+
+    cache = {}
+    def _calculate_min_time(course: int) -> int:
+        course_prereqs = prereqs[course]
+
+        if course in cache:
+            return cache[course]
+        if not course_prereqs:
+            cache[course] = time[course - 1]
+            return time[course - 1]
+
+        cost = 0
+        for course_prereq in course_prereqs:
+            cost = max(cost, _calculate_min_time(course_prereq) + time[course - 1])
+
+        cache[course] = cost
+        return cost
+
+    for course in range(1, n + 1):
+        _calculate_min_time(course)
+
+    res = 0
+    for final_course in final_courses:
+        res = max(res, cache[final_course])
+
+    return res
+
 
 
 print(minimum_time(n = 3, relations = [[1,3],[2,3]], time = [3,2,5]) == 8)
 print(minimum_time(n = 5, relations = [[1,5],[2,5],[3,5],[3,4],[4,5]], time = [1,2,3,4,5]) == 12)
+print(minimum_time(n = 11, relations=[[1,2],[3,4],[10,11],[2,5],[4,5],[5,6],[6,7],[5,8],[5,9]], time=[1,2,3,4,5,6,7,8,9,10,11]) == 25)
