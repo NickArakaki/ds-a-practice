07092de72af44fa38d5dce45024eaa49dfbc40f7
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-19 11:45:31 -0500

    passing current test cases

--- LeetCode/Backlog/844.BackspaceStringCompare.py
+++ LeetCode/Backlog/844.BackspaceStringCompare.py
@@ -22,8 +22,28 @@ def backspace_compare(s: str, t: str) -> bool:
             # add char to beginning of new str
 
     # return comparison of new_s to new_t
+    new_s, new_t = "", ""
+    s_back, t_back = 0, 0
+    for s_i in range(len(s) - 1, -1, -1):
+        char = s[s_i]
+        if char == "#":
+            s_back += 1
+        elif s_back > 0:
+            s_back -= 1
+        else:
+            new_s = char + new_s
 
-    pass
+    for t_i in range(len(t) - 1, -1, -1):
+        char = t[t_i]
+        if char == "#":
+            t_back += 1
+        elif t_back > 0:
+            t_back -= 1
+        else:
+            new_t = char + new_t
+
+    return new_s == new_t
+
 
 
 print(backspace_compare("ab#c", "ad#c") == True)
