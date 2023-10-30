c20492ec9264d9ae3658276854ad8c3d62f679d6
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-10-30 12:18:16 -0500

    add python sorted solution

--- LeetCode/Backlog/1356.SortIntegersByTheNumberOf1Bits.py
+++ LeetCode/Backlog/1356.SortIntegersByTheNumberOf1Bits.py
@@ -8,21 +8,21 @@ Return the array after sorting it.
 """
 import heapq
 
+# def sort_by_bits(arr: list[int]) -> list[int]:
+#     dp = [[] for _ in range(32)]
+#     for num in arr:
+#         count = bin(num).count("1")
+#         heapq.heappush(dp[count], num)
+
+#     res = []
+#     for heap in dp:
+#         while heap:
+#             res.append(heapq.heappop(heap))
+
+#     return res
+
 def sort_by_bits(arr: list[int]) -> list[int]:
-    dp = [[] for _ in range(32)]
-    for num in arr:
-        count = 0
-        for char in bin(num):
-            if char == "1":
-                count += 1
-        heapq.heappush(dp[count], num)
-
-    res = []
-    for heap in dp:
-        while heap:
-            res.append(heapq.heappop(heap))
-
-    return res
+    return sorted(arr, key=lambda num: (bin(num).count("1"), num))
 
 print(sort_by_bits([0,1,2,3,4,5,6,7,8]) == [0,1,2,4,8,3,5,6,7])
 print(sort_by_bits([1024,512,256,128,64,32,16,8,4,2,1]) == [1,2,4,8,16,32,64,128,256,512,1024])
