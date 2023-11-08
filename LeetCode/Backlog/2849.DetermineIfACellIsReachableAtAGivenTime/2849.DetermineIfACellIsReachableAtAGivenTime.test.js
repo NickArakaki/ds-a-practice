a7a0ec502d9afc402fdfcbcb74469c0d153c1f1f
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-08 10:35:21 -0600

    add failing test case

--- LeetCode/Backlog/2849.DetermineIfACellIsReachableAtAGivenTime/2849.DetermineIfACellIsReachableAtAGivenTime.test.js
+++ LeetCode/Backlog/2849.DetermineIfACellIsReachableAtAGivenTime/2849.DetermineIfACellIsReachableAtAGivenTime.test.js
@@ -10,4 +10,8 @@ describe("Leet Code 2894. Cell Reachable At A Given Time", () => {
   it("should return false with the input: sx = 3, sy = 1, fx = 7, fy = 3, t = 3", () => {
     expect(isReachableAtTime(3, 1, 7, 3, 3)).toBe(false);
   });
+
+  it("should return true with the input: sx = 1, sy = 1, fx = 2, fy = 2, t = 1", () => {
+    expect(isReachableAtTime(1, 1, 2, 2, 1)).toBe(true);
+  });
 });
