910c77cf338c9e4d218045f7a8c2ac2fc8d186bd
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-08 11:01:39 -0600

    bfs solution passes test cases

--- LeetCode/Backlog/2849.DetermineIfACellIsReachableAtAGivenTime/2849.DetermineIfACellIsReachableAtAGivenTime.js
+++ LeetCode/Backlog/2849.DetermineIfACellIsReachableAtAGivenTime/2849.DetermineIfACellIsReachableAtAGivenTime.js
@@ -12,14 +12,52 @@
 const isReachableAtTime = (sx, sy, fx, fy, t) => {
   /*
         Plan:
-            Calculate the euclidean distance from start point to end point
-            if the distance is greater than the time it's impossible to reach the end point before
-            time runs out
+            Breadth First Search
+            If we can reach the end point before or at t iterations,
+            we know that the end point can be reached since we can visit the same cell multiple times
 
-            euclidean distance = abs(sqrt((fx - sx)**2 + (fy - sy)**2))
+            init a queue with array of the first coordinates and 0 (number of steps taken), might not work because arrays in js are mutable
+            init a visited set with start coords
+
+            while the queue is not empty
+                pop the first el in the queue
+                if num steps taken > t return false
+                if the coords are equal to the end coords return true
+
+                get all neighbors and add to queue and visited
     */
-  const euclidDist = Math.abs(Math.sqrt((fx - sx) ** 2 + (fy - sy) ** 2));
-  return euclidDist <= t;
+
+  const queue = [[sx, sy, 0]];
+  const visited = new Set();
+  visited.add([sx, sy]);
+
+  const validNeighbors = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+    [-1, -1],
+  ];
+
+  while (queue.length) {
+    const [x, y, steps] = queue.shift();
+    if (steps > t) return false;
+    if (x === fx && y === fy) return true;
+
+    validNeighbors.forEach((neighbor) => {
+      const newX = x + neighbor[0];
+      const newY = y + neighbor[1];
+      if (!visited.has([newX, newY])) {
+        queue.push([newX, newY, steps + 1]);
+        visited.add([newX, newY]);
+      }
+    });
+  }
+
+  return false;
 };
 
 module.exports.isReachableAtTime = isReachableAtTime;
