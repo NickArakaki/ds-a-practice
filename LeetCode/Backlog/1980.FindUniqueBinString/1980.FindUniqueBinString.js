461848424e3ce7f77321f999169d58111b85582f
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-16 12:26:54 -0600

    add brute force plan

--- LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
+++ LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
@@ -5,6 +5,12 @@
 */
 
 const findDifferentBinString = (nums) => {
+  // get max of possible nums, 2**(nums[0].length - 1)
+  //   const max = 2 ** nums[0].length - 1;
+  // convert each num to int and store in set
+  // iterate from 0 -> max
+  // if num not in num set
+  // convert to bin string and return
   return;
 };
 
