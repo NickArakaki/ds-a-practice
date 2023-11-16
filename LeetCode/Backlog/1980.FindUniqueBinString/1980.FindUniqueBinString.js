183131923458836cf0a6f94fc62caf104be87f15
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-16 12:41:00 -0600

    add plan for more optimal solution

--- LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
+++ LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
@@ -4,19 +4,24 @@
     appear in nums. If there are multiple answers, you may return any of them.
 */
 
-const findDifferentBinString = (nums) => {
-  // get max of possible nums, 2**(nums[0].length - 1)
-  const numBits = nums[0].length;
-  // convert each num to int and store in set
-  const numSet = new Set(nums.map((num) => parseInt(num, 2)));
-  const max = 2 ** numBits - 1;
+// const findDifferentBinString = (nums) => {
+//   // get max of possible nums, 2**(nums[0].length - 1)
+//   const numBits = nums[0].length;
+//   // convert each num to int and store in set
+//   const numSet = new Set(nums.map((num) => parseInt(num, 2)));
+//   const max = 2 ** numBits - 1;
 
-  for (let curNum = 0; curNum <= max; curNum++) {
-    if (!numSet.has(curNum)) {
-      let binString = curNum.toString(2);
-      return "0".repeat(numBits - binString.length) + binString;
-    }
-  }
+//   for (let curNum = 0; curNum <= max; curNum++) {
+//     if (!numSet.has(curNum)) {
+//       let binString = curNum.toString(2);
+//       return "0".repeat(numBits - binString.length) + binString;
+//     }
+//   }
+// };
+
+const findDifferentBinString = (nums) => {
+  // get opposite of bit at index i for nums[i]
+  // return built up string
 };
 
 module.exports.findDifferentBinString = findDifferentBinString;
