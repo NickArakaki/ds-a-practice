41bca02afe42201e86a1425e131124c4cb55408c
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-16 12:43:29 -0600

    add linear time solution

--- LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
+++ LeetCode/Backlog/1980.FindUniqueBinString/1980.FindUniqueBinString.js
@@ -21,7 +21,14 @@
 
 const findDifferentBinString = (nums) => {
   // get opposite of bit at index i for nums[i]
+  let res = "";
+
+  for (let i = 0; i < nums.length; i++) {
+    res += nums[i][i] === "0" ? "1" : "0";
+  }
+
   // return built up string
+  return res;
 };
 
 module.exports.findDifferentBinString = findDifferentBinString;
