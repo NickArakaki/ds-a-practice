1a50127132459cc1b463d498f4d00c7e0230448f
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-28 12:11:42 -0600

    update test cases to reflect test cases on leetcode

--- LeetCode/Backlog/27.RemoveElement/27.RemoveElement.test.js
+++ LeetCode/Backlog/27.RemoveElement/27.RemoveElement.test.js
@@ -2,10 +2,16 @@ const { removeElement } = require("./27.RemoveElement");
 
 describe("Leetcode 27. Remove Element", () => {
   it("should return [2,2,_,_] given the input nums=[3,2,2,3], val = 3", () => {
-    expect(removeElement([3, 2, 2, 3], 3)).toEqual([2, 2]);
+    const arr = [3, 2, 2, 3];
+    const k = removeElement(arr, 3);
+    expect(k).toBe(2);
+    expect(arr.slice(0, k)).toEqual([2, 2]);
   });
 
-  it("should return [0,1,4,0,3,_,_,_] given the input nums=[0,1,2,2,3,0,4,2], val = 2", () => {
-    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toEqual([0, 1, 3, 0, 4]);
+  it("should return [0, 1, 3, 0, 4, _, _, _] given the input nums=[0,1,2,2,3,0,4,2], val = 2", () => {
+    const arr = [0, 1, 2, 2, 3, 0, 4, 2];
+    const k = removeElement(arr, 2);
+    expect(k).toBe(5);
+    expect(arr.slice(0, k)).toEqual([0, 1, 3, 0, 4]);
   });
 });
