638dda71d9a8791f9ea968f60bc84339c3b1a5c4
Nicholas Arakaki <nicholas.w.arakaki@gmail.com>
2023-11-28 11:55:47 -0600

    passing all test cases for remove element

--- LeetCode/Backlog/27.RemoveElement/27.RemoveElement.test.js
+++ LeetCode/Backlog/27.RemoveElement/27.RemoveElement.test.js
@@ -2,19 +2,10 @@ const { removeElement } = require("./27.RemoveElement");
 
 describe("Leetcode 27. Remove Element", () => {
   it("should return [2,2,_,_] given the input nums=[3,2,2,3], val = 3", () => {
-    expect(removeElement([3, 2, 2, 3], 3)).toBe([2, 2]);
+    expect(removeElement([3, 2, 2, 3], 3)).toEqual([2, 2]);
   });
 
   it("should return [0,1,4,0,3,_,_,_] given the input nums=[0,1,2,2,3,0,4,2], val = 2", () => {
-    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toBe([
-      0,
-      1,
-      4,
-      0,
-      3,
-      _,
-      _,
-      _,
-    ]);
+    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toEqual([0, 1, 3, 0, 4]);
   });
 });
